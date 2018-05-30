1aa10b5e4960870677e4d2ab01cbde9b10947f62
Sandeep <sandeep.patel@bypt.in>
2018-05-30 18:24:07 +0530

    Code refactoring

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -57,7 +57,7 @@ public final class Constants {
         private static final String FILENAME_NETWORK_SUFFIX = NETWORK_PARAMETERS.getId()
                 .equals(NetworkParameters.ID_MAINNET) ? "" : "-testnet";
 
-        /**Filename of the wallet.*/
+        /** Filename of the wallet. */
         public static final String WALLET_FILENAME_PROTOBUF = "wallet-protobuf" + FILENAME_NETWORK_SUFFIX;
 
         /**How often the wallet is autosaved.*/
