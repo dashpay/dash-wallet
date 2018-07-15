1d10b208597e72b1410ab13e8cb017a55256f617
HashEngineering <hashengineeringsolutions@gmail.com>
2018-07-14 19:36:15 -0700

    PaymentIntent:  InstantSend is OFF by default

--- wallet/src/de/schildbach/wallet/data/PaymentIntent.java
+++ wallet/src/de/schildbach/wallet/data/PaymentIntent.java
@@ -185,7 +185,6 @@ public final class PaymentIntent implements Parcelable {
         this.payeeData = payeeData;
         this.paymentRequestUrl = paymentRequestUrl;
         this.paymentRequestHash = paymentRequestHash;
-        this.useInstantX = true;
     }
 
     public PaymentIntent(@Nullable final Standard standard, @Nullable final String payeeName, @Nullable final String payeeVerifiedBy,
@@ -226,8 +225,6 @@ public final class PaymentIntent implements Parcelable {
         final String paymentRequestHashStr = (String) bitcoinUri.getParameterByName("h");
         final byte[] paymentRequestHash = paymentRequestHashStr != null ? base64UrlDecode(paymentRequestHashStr) : null;
         boolean useInstantSend = bitcoinUri.getRequestInstantSend();
-        if(bitcoinUri.getParameterByName("is") == null)
-            useInstantSend = true;
 
         return new PaymentIntent(PaymentIntent.Standard.BIP21, null, null, outputs, bitcoinUri.getLabel(),
                 bluetoothMac != null ? "bt:" + bluetoothMac : null, null, bitcoinUri.getPaymentRequestUrl(),
