92a5dfe4245fd631b0b8cb6e3d63a22ec0efb542
Samuel Barbosa <sambarbosaa@gmail.com>
2018-09-03 22:39:42 -0500

    Set exchange rate on received transactions
    * Show value of received transactions in local currency on transactions list and notifications
    * Don't set exchange rate on old transactions or when blockchain is being reset (30 min or older)

--- wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.java
+++ wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.java
@@ -57,6 +57,7 @@ import org.bitcoinj.net.discovery.PeerDiscoveryException;
 import org.bitcoinj.store.BlockStore;
 import org.bitcoinj.store.BlockStoreException;
 import org.bitcoinj.store.SPVBlockStore;
+import org.bitcoinj.utils.ExchangeRate;
 import org.bitcoinj.utils.MonetaryFormat;
 import org.bitcoinj.utils.Threading;
 import org.bitcoinj.wallet.Wallet;
@@ -133,6 +134,7 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
     private static final int MAX_HISTORY_SIZE = Math.max(IDLE_TRANSACTION_TIMEOUT_MIN, IDLE_BLOCK_TIMEOUT_MIN);
     private static final long APPWIDGET_THROTTLE_MS = DateUtils.SECOND_IN_MILLIS;
     private static final long BLOCKCHAIN_STATE_BROADCAST_THROTTLE_MS = DateUtils.SECOND_IN_MILLIS;
+    private static final long TX_EXCHANGE_RATE_TIME_THRESHOLD_MS = TimeUnit.MINUTES.toMillis(30);
 
     private static final Logger log = LoggerFactory.getLogger(BlockchainServiceImpl.class);
 
@@ -146,9 +148,22 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
         @Override
         public void onCoinsReceived(final Wallet wallet, final Transaction tx, final Coin prevBalance,
                 final Coin newBalance) {
-            transactionsReceived.incrementAndGet();
 
             final int bestChainHeight = blockChain.getBestChainHeight();
+            final boolean replaying = bestChainHeight < config.getBestChainHeightEver();
+
+            long now = new Date().getTime();
+            long blockChainHeadTime = blockChain.getChainHead().getHeader().getTime().getTime();
+            boolean insideTxExchangeRateTimeThreshold = (now - blockChainHeadTime) < TX_EXCHANGE_RATE_TIME_THRESHOLD_MS;
+
+            final ExchangeRate exchangeRate = config.getCachedExchangeRate().rate;
+            if (tx.getExchangeRate() == null && exchangeRate != null && !replaying && insideTxExchangeRateTimeThreshold) {
+                tx.setExchangeRate(exchangeRate);
+                application.saveWallet();
+            }
+
+            transactionsReceived.incrementAndGet();
+
 
             final Address address = WalletUtils.getWalletAddressOfReceived(tx, wallet);
             final Coin amount = tx.getValue(wallet);
@@ -158,11 +173,10 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
                 @Override
                 public void run() {
                     final boolean isReceived = amount.signum() > 0;
-                    final boolean replaying = bestChainHeight < config.getBestChainHeightEver();
                     final boolean isReplayedTx = confidenceType == ConfidenceType.BUILDING && replaying;
 
                     if (isReceived && !isReplayedTx)
-                        notifyCoinsReceived(address, amount);
+                        notifyCoinsReceived(address, amount, exchangeRate);
                 }
             });
         }
@@ -174,7 +188,8 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
         }
     };
 
-    private void notifyCoinsReceived(@Nullable final Address address, final Coin amount) {
+    private void notifyCoinsReceived(@Nullable final Address address, final Coin amount,
+                                     @Nullable ExchangeRate exchangeRate) {
         if (notificationCount == 1)
             nm.cancel(Constants.NOTIFICATION_ID_COINS_RECEIVED);
 
@@ -186,7 +201,14 @@ public class BlockchainServiceImpl extends android.app.Service implements Blockc
         final MonetaryFormat btcFormat = config.getFormat();
 
         final String packageFlavor = application.applicationPackageFlavor();
-        final String msgSuffix = packageFlavor != null ? " [" + packageFlavor + "]" : "";
+        String msgSuffix = packageFlavor != null ? " [" + packageFlavor + "]" : "";
+
+        if (exchangeRate != null) {
+            exchangeRate.coinToFiat(amount);
+            MonetaryFormat format = Constants.LOCAL_FORMAT.code(0,
+                    Constants.PREFIX_ALMOST_EQUAL_TO + exchangeRate.fiat.getCurrencyCode());
+            msgSuffix += " " + format.format(exchangeRate.coinToFiat(amount));
+        }
 
         final String tickerMsg = getString(R.string.notification_coins_received_msg, btcFormat.format(amount))
                 + msgSuffix;
