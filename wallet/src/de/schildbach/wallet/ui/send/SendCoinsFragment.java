597b2dc8138ac9f2671ceb5dd35be9af0aec4a6a
Tomasz Ludek <tomasz@dash.org>
2019-03-28 17:12:27 +0100

    After scanning a QR code which requires IS, the IS upgrade checkbox in Send Screen is checked automatically.

--- wallet/src/de/schildbach/wallet/ui/send/SendCoinsFragment.java
+++ wallet/src/de/schildbach/wallet/ui/send/SendCoinsFragment.java
@@ -1685,6 +1685,10 @@ public final class SendCoinsFragment extends Fragment {
                 }
                 instantSendInfo.setVisibility(View.VISIBLE);
                 instantXenable.setVisibility(View.VISIBLE);
+                if (paymentIntent.getUseInstantSend()) {
+                    instantXenable.setEnabled(false);
+                    instantXenable.setChecked(true);
+                }
                 break;
             }
             case REGULAR_PAYMENT: {
@@ -1822,6 +1826,8 @@ public final class SendCoinsFragment extends Fragment {
                         directPaymentEnableView.setChecked(bluetoothAdapter != null && bluetoothAdapter.isEnabled());
                     else if (paymentIntent.isHttpPaymentUrl())
                         directPaymentEnableView.setChecked(!Constants.BUG_OPENSSL_HEARTBLEED);
+
+                    instantXenable.setChecked(paymentIntent.getUseInstantSend());
 
                     requestFocusFirst();
                     updateView();
