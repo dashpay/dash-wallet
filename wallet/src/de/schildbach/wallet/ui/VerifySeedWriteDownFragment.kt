9242b3b69ba63400cd3be75572f4a1b1357c0e8b
Samuel Barbosa <sambarbosaa@gmail.com>
2019-08-14 07:29:28 -0500

    Added copyright information.

--- wallet/src/de/schildbach/wallet/ui/VerifySeedWriteDownFragment.kt
+++ wallet/src/de/schildbach/wallet/ui/VerifySeedWriteDownFragment.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Dash Core Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.schildbach.wallet.ui
 
 import android.os.Bundle
@@ -8,7 +24,6 @@ import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import de.schildbach.wallet_test.R
 import kotlinx.android.synthetic.main.verify_seed_write_down.*
-import java.lang.StringBuilder
 
 /**
  * @author Samuel Barbosa
