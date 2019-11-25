4df1f21c2d6cc3444ea615ae57bf024ab78b653a
Den <dlilienblum@gmail.com>
2019-11-25 16:56:47 +0400

    keep old code in WalletActivity

--- wallet/src/de/schildbach/wallet/ui/WalletActivity.java
+++ wallet/src/de/schildbach/wallet/ui/WalletActivity.java
@@ -35,9 +35,12 @@ import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.telephony.TelephonyManager;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -69,6 +72,7 @@ import org.dash.wallet.common.Configuration;
 import org.dash.wallet.common.ui.DialogBuilder;
 import org.dash.wallet.integration.uphold.data.UpholdClient;
 import org.dash.wallet.integration.uphold.ui.UpholdAccountActivity;
+import org.dash.wallet.integration.uphold.ui.UpholdSplashActivity;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -121,6 +125,7 @@ public final class WalletActivity extends AbstractBindServiceActivity
     private FingerprintHelper fingerprintHelper;
 
     private DrawerLayout viewDrawer;
+    private View viewFakeForSafetySubmenu;
 
     private Handler handler = new Handler();
 
@@ -290,7 +295,11 @@ public final class WalletActivity extends AbstractBindServiceActivity
         final NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        viewFakeForSafetySubmenu = new View(this);
+        viewFakeForSafetySubmenu.setVisibility(View.GONE);
         viewDrawer = findViewById(R.id.drawer_layout);
+        viewDrawer.addView(viewFakeForSafetySubmenu);
+        registerForContextMenu(viewFakeForSafetySubmenu);
     }
 
     @Override
@@ -600,6 +609,11 @@ public final class WalletActivity extends AbstractBindServiceActivity
         }
     }
 
+    private void enableFingerprint() {
+        config.setRemindEnableFingerprint(true);
+        UnlockWalletDialogFragment.show(getSupportFragmentManager());
+    }
+
     @Override
     protected Dialog onCreateDialog(final int id, final Bundle args) {
         if (id == DIALOG_BACKUP_WALLET_PERMISSION)
@@ -896,6 +910,62 @@ public final class WalletActivity extends AbstractBindServiceActivity
         }
     }
 
+//    TODO: remove when Security screen is done
+//    @Override
+//    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+//        super.onCreateContextMenu(menu, v, menuInfo);
+//        MenuInflater inflater = getMenuInflater();
+//        if (v == viewFakeForSafetySubmenu) {
+//            inflater.inflate(R.menu.wallet_safety_options, menu);
+//
+//            final String externalStorageState = Environment.getExternalStorageState();
+//
+//            menu.findItem(R.id.wallet_options_restore_wallet).setEnabled(
+//                    Environment.MEDIA_MOUNTED.equals(externalStorageState) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(externalStorageState));
+//            menu.findItem(R.id.wallet_options_backup_wallet).setEnabled(Environment.MEDIA_MOUNTED.equals(externalStorageState));
+//            menu.findItem(R.id.wallet_options_encrypt_keys).setTitle(
+//                    wallet.isEncrypted() ? R.string.wallet_options_encrypt_keys_change : R.string.wallet_options_encrypt_keys_set);
+//
+//            boolean showFingerprintOption = fingerprintHelper != null && !fingerprintHelper.isFingerprintEnabled();
+//            menu.findItem(R.id.wallet_options_enable_fingerprint).setVisible(showFingerprintOption);
+//        }
+//    }
+
+//    TODO: remove when Security screen is done
+//    @Override
+//    public boolean onContextItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.wallet_options_safety:
+//                HelpDialogFragment.page(getSupportFragmentManager(), R.string.help_safety);
+//                return true;
+//
+//            case R.id.wallet_options_backup_wallet:
+//                handleBackupWallet();
+//                return true;
+//
+//            case R.id.wallet_options_restore_wallet:
+//                handleRestoreWallet();
+//                return true;
+//
+//            case R.id.wallet_options_encrypt_keys:
+//                handleEncryptKeys();
+//                return true;
+//            case R.id.wallet_options_backup_wallet_to_seed:
+//                handleBackupWalletToSeed();
+//                return true;
+//
+//            case R.id.wallet_options_restore_wallet_from_seed:
+//                handleRestoreWalletFromSeed();
+//                return true;
+//
+//            case R.id.wallet_options_enable_fingerprint:
+//                enableFingerprint();
+//                return true;
+//        }
+//
+//        return super.onContextItemSelected(item);
+//    }
+
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
@@ -911,6 +981,9 @@ public final class WalletActivity extends AbstractBindServiceActivity
             SweepWalletActivity.start(this);
         } else if (id == R.id.nav_network_monitor) {
             startActivity(new Intent(this, NetworkMonitorActivity.class));
+//        TODO: remove when Security screen is done
+//        } else if (id == R.id.nav_safety) {
+//            openContextMenu(viewFakeForSafetySubmenu);
         } else if (id == R.id.nav_settings) {
             startActivity(new Intent(this, PreferenceActivity.class));
         } else if (id == R.id.nav_disconnect) {
