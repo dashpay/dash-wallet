9c8b3f6403923e6c0852578d3eeeabfbd7f98134
Den <dlilienblum@gmail.com>
2019-12-17 17:26:08 +0300

    security level; ui connected to configuration

--- common/src/main/java/org/dash/wallet/common/Configuration.java
+++ common/src/main/java/org/dash/wallet/common/Configuration.java
@@ -58,6 +58,10 @@ public class Configuration {
     public static final String PREFS_KEY_DISCLAIMER = "disclaimer";
     private static final String PREFS_KEY_LABS_QR_PAYMENT_REQUEST = "labs_qr_payment_request";
     private static final String PREFS_KEY_PREVIOUS_VERSION = "previous_version";
+    private static final String PREFS_KEY_AUTO_LOGOUT_ENABLED = "auto_logout_enabled";
+    private static final String PREFS_KEY_AUTO_LOGOUT_MINUTES = "auto_logout_minutes";
+    private static final String PREFS_KEY_SPENDING_CONFIRMATION_ENABLED = "spending_confirmation_enabled";
+    private static final String PREFS_KEY_SPENDING_CONFIRMATION_LIMIT = "spending_confirmation_limit";
 
     private static final String PREFS_KEY_LAST_VERSION = "last_version";
     private static final String PREFS_KEY_LAST_USED = "last_used";
@@ -189,6 +193,38 @@ public class Configuration {
 
     public void setRemindBalance(final boolean remindBalance) {
         prefs.edit().putBoolean(PREFS_KEY_REMIND_BALANCE, remindBalance).apply();
+    }
+
+    public boolean getAutoLogoutEnabled() {
+        return prefs.getBoolean(PREFS_KEY_AUTO_LOGOUT_ENABLED, true);
+    }
+
+    public void setAutoLogoutEnabled(final boolean enabled) {
+        prefs.edit().putBoolean(PREFS_KEY_AUTO_LOGOUT_ENABLED, enabled).apply();
+    }
+
+    public int getAutoLogoutMinutes() {
+        return prefs.getInt(PREFS_KEY_AUTO_LOGOUT_MINUTES, 1);
+    }
+
+    public void setAutoLogoutMinutes(final int minutes) {
+        prefs.edit().putInt(PREFS_KEY_AUTO_LOGOUT_MINUTES, minutes).apply();
+    }
+
+    public boolean getSpendingConfirmationEnabled() {
+        return prefs.getBoolean(PREFS_KEY_SPENDING_CONFIRMATION_ENABLED, true);
+    }
+
+    public void setSpendingConfirmationEnabled(final boolean enabled) {
+        prefs.edit().putBoolean(PREFS_KEY_SPENDING_CONFIRMATION_ENABLED, enabled).apply();
+    }
+
+    public float getSpendingConfirmationLimit() {
+        return prefs.getFloat(PREFS_KEY_SPENDING_CONFIRMATION_LIMIT, 0.5f);
+    }
+
+    public void setSpendingConfirmationLimit(final float limit) {
+        prefs.edit().putFloat(PREFS_KEY_SPENDING_CONFIRMATION_LIMIT, limit).apply();
     }
 
     public boolean remindBackup() {
--- wallet/src/de/schildbach/wallet/ui/AdvancedSecurityActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/AdvancedSecurityActivity.kt
@@ -18,13 +18,17 @@ package de.schildbach.wallet.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import de.schildbach.wallet_test.R
 import kotlinx.android.synthetic.main.activity_advanced_security.*
+import java.util.concurrent.TimeUnit
+
+enum class SecurityLevel {
+    NONE, LOW, MEDIUM, HIGH, VERY_HIGH
+}
 
 class AdvancedSecurityActivity : BaseMenuActivity() {
-    private var autoLogout = false
-    private var spendingConfirmation = false
-
     override fun getLayoutId(): Int {
         return R.layout.activity_advanced_security
     }
@@ -35,18 +39,152 @@ class AdvancedSecurityActivity : BaseMenuActivity() {
         security_icon.setImageDrawable(resources.getDrawable(R.drawable.security_filled_blue))
 
         auto_logout_switch.setOnCheckedChangeListener { _, enabled ->
-            auto_logout_group.visibility = if (enabled) View.VISIBLE  else View.GONE
+            configuration.autoLogoutEnabled = enabled
+            updateView()
         }
 
         spending_confirmation_switch.setOnCheckedChangeListener { _, enabled ->
-            spending_confirmation_group.visibility = if (enabled) View.VISIBLE  else View.GONE
+            configuration.spendingConfirmationEnabled = enabled
+            updateView()
         }
 
+        auto_logout_seekbar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                configuration.autoLogoutMinutes = when(auto_logout_seekbar.progress) {
+                    0 -> 0
+                    1 -> 1
+                    2 -> 5
+                    3 -> 60
+                    else -> TimeUnit.HOURS.toMinutes(24).toInt()
+                }
+                updateView()
+            }
+        })
+        biometric_limit_seekbar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar?) { }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) { }
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                configuration.spendingConfirmationLimit = when(biometric_limit_seekbar.progress) {
+                    0 -> 0f
+                    1 -> 0.1f
+                    2 -> 0.5f
+                    3 -> 1f
+                    else -> 5f
+                }
+                updateView()
+            }
+        })
+
+        updateView()
         setTitle(R.string.security_title)
     }
 
+    private fun getSecurityLevel(): SecurityLevel {
+        return if (configuration.autoLogoutEnabled) {
+            if (configuration.spendingConfirmationEnabled) {
+                if (configuration.autoLogoutMinutes == 0) {
+                    SecurityLevel.VERY_HIGH
+                } else {
+                    SecurityLevel.HIGH
+                }
+            } else {
+                if (configuration.autoLogoutMinutes >= 60) {
+                    SecurityLevel.LOW
+                } else {
+                    SecurityLevel.MEDIUM
+                }
+            }
+        } else {
+            if (configuration.spendingConfirmationEnabled) {
+                SecurityLevel.MEDIUM
+            } else {
+                SecurityLevel.NONE
+            }
+        }
+    }
+
     private fun updateView() {
+        // Auto Logout group
+        auto_logout_switch.isChecked = configuration.autoLogoutEnabled
+        if (configuration.autoLogoutEnabled) {
+            auto_logout_group.visibility = View.VISIBLE
+            logout_after_time.text = getString(when(configuration.autoLogoutMinutes) {
+                0 -> R.string.immediately
+                1 -> R.string.one_minute
+                5 -> R.string.five_minute
+                60 -> R.string.one_hour
+                else -> R.string.one_day
+            })
+            auto_logout_seekbar.progress = when(configuration.autoLogoutMinutes) {
+                0 -> 0
+                1 -> 1
+                5 -> 2
+                60 -> 3
+                else -> 4
+            }
+        } else {
+            auto_logout_group.visibility = View.GONE
+        }
 
+        // Spending Confirmation group
+        spending_confirmation_switch.isChecked = configuration.spendingConfirmationEnabled
+        if (configuration.spendingConfirmationEnabled) {
+            spending_confirmation_group.visibility = View.VISIBLE
+            biometric_limit_value.text = configuration.spendingConfirmationLimit
+                    .toString().removeSuffix(".0")
+            biometric_limit_seekbar.progress = when(configuration.spendingConfirmationLimit) {
+                0f -> 0
+                .1f -> 1
+                .5f -> 2
+                1f -> 3
+                else -> 4
+            }
+            if (configuration.spendingConfirmationLimit == 0f) {
+                spending_confirmation_hint.text = getText(R.string.spending_confirmation_hint_zero)
+                spending_confirmation_hint_dash.visibility = View.GONE
+                spending_confirmation_hint_amount.visibility = View.GONE
+            } else {
+                spending_confirmation_hint.text = getText(R.string.spending_confirmation_hint_above_zero)
+                spending_confirmation_hint_amount.text = biometric_limit_value.text
+                spending_confirmation_hint_dash.visibility = View.VISIBLE
+                spending_confirmation_hint_amount.visibility = View.VISIBLE
+            }
+        } else {
+            spending_confirmation_group.visibility = View.GONE
+        }
 
+        // Security Level
+        val securityLevel = getSecurityLevel()
+        security_level.text = getString(when(securityLevel) {
+            SecurityLevel.NONE -> R.string.security_none
+            SecurityLevel.LOW -> R.string.security_low
+            SecurityLevel.MEDIUM -> R.string.security_medium
+            SecurityLevel.HIGH -> R.string.security_high
+            else -> R.string.security_very_high
+        })
+        security_level.setTextColor(when(securityLevel) {
+            SecurityLevel.NONE -> R.color.dash_red
+            SecurityLevel.LOW -> R.color.dash_orange
+            SecurityLevel.MEDIUM -> R.color.dash_orange
+            SecurityLevel.HIGH -> R.color.dash_blue
+            else -> R.color.dash_green
+        })
+        security_icon.setImageResource(when(securityLevel) {
+            SecurityLevel.NONE -> R.drawable.security_filled_red
+            SecurityLevel.LOW -> R.drawable.security_filled_orange
+            SecurityLevel.MEDIUM -> R.drawable.security_filled_orange
+            SecurityLevel.HIGH -> R.drawable.security_filled_blue
+            else -> R.drawable.security_filled_green
+        })
+    }
+
+    fun resetToDefault(view: View) {
+        configuration.autoLogoutEnabled = true
+        configuration.autoLogoutMinutes = 1
+        configuration.spendingConfirmationEnabled = true
+        configuration.spendingConfirmationLimit = .5f
+        updateView()
     }
 }
