82d599e93069ea3c5155efc57437d76e49bbaf98
HashEngineering <hashengineeringsolutions@gmail.com>
2020-01-08 22:06:36 -0800

    Undo changes to VerifySeedActivity since ViewSeedActivity will be used

--- wallet/src/de/schildbach/wallet/ui/VerifySeedActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/VerifySeedActivity.kt
@@ -32,18 +32,15 @@ class VerifySeedActivity : AppCompatActivity(), VerifySeedActions {
     companion object {
 
         private const val EXTRA_SEED = "extra_seed"
-        private const val EXTRA_VIEW_ONLY = "extra_write_down"
 
-        fun createIntent(context: Context, seed: Array<String>, viewOnly: Boolean = false): Intent {
+        fun createIntent(context: Context, seed: Array<String>): Intent {
             val intent = Intent(context, VerifySeedActivity::class.java)
             intent.putExtra(EXTRA_SEED, seed)
-            intent.putExtra(EXTRA_VIEW_ONLY, viewOnly)
             return intent
         }
     }
 
     private var seed: Array<String> = arrayOf()
-    private var viewOnly = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,19 +52,9 @@ class VerifySeedActivity : AppCompatActivity(), VerifySeedActions {
             throw IllegalStateException("This activity needs to receive a String[] Intent Extra " +
                     "containing the recovery seed.")
         }
-        viewOnly = if(intent.extras.containsKey(EXTRA_VIEW_ONLY)) {
-            intent.extras!!.getBoolean(EXTRA_VIEW_ONLY)!!
-        } else {
-            false
-        }
 
-        if(viewOnly) {
-            supportFragmentManager.beginTransaction().add(R.id.container,
-                    VerifySeedWriteDownFragment.newInstance(seed)).commit()
-        } else {
-            supportFragmentManager.beginTransaction().add(R.id.container,
+        supportFragmentManager.beginTransaction().add(R.id.container,
                     VerifySeedSecureNowFragment.newInstance()).commit()
-        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -91,12 +78,8 @@ class VerifySeedActivity : AppCompatActivity(), VerifySeedActions {
     }
 
     override fun onVerifyWriteDown() {
-        if(viewOnly) {
-            goHome()
-        } else {
             supportFragmentManager.beginTransaction().replace(R.id.container,
                     VerifySeedConfirmFragment.newInstance(seed)).commit()
-        }
     }
 
     override fun onSeedVerified() {
