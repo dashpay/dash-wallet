4325bc425be1216cddcb58359b81125508973d69
Den <dlilienblum@gmail.com>
2020-01-16 19:44:02 +0300

    Lock Screen back close the app

--- wallet/src/de/schildbach/wallet/ui/LockScreenActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/LockScreenActivity.kt
@@ -295,8 +295,4 @@ class LockScreenActivity : SendCoinsQrActivity() {
         }
         dialogBuilder.show()
     }
-
-    override fun onBackPressed() {
-        // block closing activity by back button
-    }
 }
