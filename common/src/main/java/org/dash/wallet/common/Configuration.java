50254adaf696f2b8f3284912ff37deb4c62d3148
Tomasz Ludek <tomasz@dash.org>
2020-02-10 14:48:01 +0100

    Fixed the issue with the app name being "ui-components" instead of Dash Wallet in prod builds

--- common/src/main/java/org/dash/wallet/common/Configuration.java
+++ common/src/main/java/org/dash/wallet/common/Configuration.java
@@ -176,8 +176,7 @@ public class Configuration {
     }
 
     public Uri getBlockExplorer() {
-        return Uri.parse(prefs.getString(PREFS_KEY_BLOCK_EXPLORER,
-                res.getStringArray(R.array.preferences_block_explorer_values)[0]));
+        return Uri.parse(prefs.getString(PREFS_KEY_BLOCK_EXPLORER, null));
     }
 
     public boolean remindBalance() {
