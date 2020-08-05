62e963fc7c3824ecb0c19c1efbc810cb0fdb0ae8
HashEngineering <hashengineeringsolutions@gmail.com>
2020-08-05 15:40:36 -0700

    Prioritize the contacts that payments are sent to

--- wallet/src/de/schildbach/wallet/ui/dashpay/FrequentContactsLiveData.kt
+++ wallet/src/de/schildbach/wallet/ui/dashpay/FrequentContactsLiveData.kt
@@ -3,7 +3,6 @@ package de.schildbach.wallet.ui.dashpay
 import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import de.schildbach.wallet.WalletApplication
-import de.schildbach.wallet.data.NotificationItem
 import de.schildbach.wallet.data.UsernameSearchResult
 import de.schildbach.wallet.data.UsernameSortOrderBy
 import de.schildbach.wallet.livedata.Resource
@@ -11,12 +10,14 @@ import de.schildbach.wallet.livedata.Status
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import org.dashevo.dashpay.BlockchainIdentity
 import java.util.*
 
 class FrequentContactsLiveData(val walletApplication: WalletApplication, private val platformRepo: PlatformRepo) : LiveData<Resource<List<UsernameSearchResult>>>(), OnContactsUpdated {
 
     companion object {
         const val TIMESPAN: Long = DateUtils.DAY_IN_MILLIS * 90 // 90 days
+        const val TOP_CONTACT_COUNT = 4
     }
 
     private var listening = false
@@ -50,32 +51,17 @@ class FrequentContactsLiveData(val walletApplication: WalletApplication, private
 
     fun getFrequentContacts() {
         GlobalScope.launch(Dispatchers.IO) {
-            val results = arrayListOf<UsernameSearchResult>()
-            val contactScores = hashMapOf<String, Int>()
             val contactRequests = platformRepo.searchContacts("", UsernameSortOrderBy.DATE_ADDED)
             when (contactRequests.status) {
                 Status.SUCCESS -> {
                     val blockchainIdentity = platformRepo.getBlockchainIdentity() ?: return@launch
                     val threeMonthsAgo = Date().time - TIMESPAN
-                    val contactNames = arrayListOf<String>()
 
-                    contactRequests.data!!.forEach {
-                        val transactions = blockchainIdentity.getContactTransactions(it.fromContactRequest!!.userId)
-                        var count = 0
+                    val results = getTopContacts(contactRequests.data!!, listOf(), blockchainIdentity, threeMonthsAgo, true)
 
-                        for (tx in transactions) {
-                            if (tx.updateTime.time > threeMonthsAgo)
-                                count++
-                        }
-                        contactScores[it.username] = count
-                        contactNames.add(it.username)
-                    }
-
-                    // determine users with top 4 scores
-                    contactNames.sortByDescending { contactScores[it] }
-                    for (i in 0 until 4) {
-                        val username = contactNames[i]
-                        results.add(contactRequests.data.find { it.username == username }!!)
+                    if (results.size < TOP_CONTACT_COUNT) {
+                        val moreResults = getTopContacts(contactRequests.data!!, results, blockchainIdentity, threeMonthsAgo, false)
+                        results.addAll(moreResults)
                     }
 
                     postValue(Resource.success(results))
@@ -83,5 +69,45 @@ class FrequentContactsLiveData(val walletApplication: WalletApplication, private
                 else -> postValue(contactRequests)
             }
         }
+    }
+
+    private fun getTopContacts(items: List<UsernameSearchResult>,
+                       ignore: List<UsernameSearchResult>,
+                       blockchainIdentity: BlockchainIdentity,
+                       threeMonthsAgo: Long,
+                       sent: Boolean
+    ) : ArrayList<UsernameSearchResult> {
+        val results = arrayListOf<UsernameSearchResult>()
+        val contactScores = hashMapOf<String, Int>()
+        val contactIds = arrayListOf<String>()
+        items.forEach {
+            val transactions = blockchainIdentity.getContactTransactions(it.fromContactRequest!!.userId)
+            var count = 0
+
+            for (tx in transactions) {
+                val txValue = tx.getValue(walletApplication.wallet)
+                if ((sent && txValue.isNegative) || (!sent && txValue.isPositive)) {
+                    if (tx.updateTime.time > threeMonthsAgo) {
+                        count++
+                    }
+                }
+            }
+            contactScores[it.fromContactRequest!!.userId] = count
+            contactIds.add(it.fromContactRequest!!.userId)
+        }
+
+        // determine users with top TOP_CONTACT_COUNT non-zero scores
+        // if ignore has some items, then find TOP_CONTACT_COUNT - ignore.size
+        contactIds.sortByDescending { contactScores[it] }
+        var count = 0
+        for (id in contactIds) {
+            if (contactScores[id] != 0 && ignore.find { it.fromContactRequest!!.userId == id } == null) {
+                results.add(items.find { it.fromContactRequest!!.userId == id }!!)
+                count++
+                if (count == TOP_CONTACT_COUNT - ignore.size)
+                    break
+            }
+        }
+        return results
     }
 }
