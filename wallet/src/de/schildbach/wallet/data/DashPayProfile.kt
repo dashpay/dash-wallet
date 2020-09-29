cc9449946f8dfd05c38212706579e6d22a14952a
HashEngineering <hashengineeringsolutions@gmail.com>
2020-09-29 09:37:21 -0700

    Refactor DashPayProfile

--- wallet/src/de/schildbach/wallet/data/DashPayProfile.kt
+++ wallet/src/de/schildbach/wallet/data/DashPayProfile.kt
@@ -16,18 +16,28 @@ data class DashPayProfile(@PrimaryKey val userId: String,
                           val createdAt: Long = 0,
                           val updatedAt: Long = 0) : Parcelable {
     companion object {
-        fun fromDocument(document: Document, username: String): DashPayProfile? {
-            return try {
-                DashPayProfile(document.ownerId,
-                        username,
-                        document.data["displayName"] as String,
-                        document.data["publicMessage"] as String,
-                        document.data["avatarUrl"] as String,
-                        if (document.createdAt != null) document.createdAt!! else 0L,
-                        if (document.updatedAt != null) document.updatedAt!! else 0L)
-            } catch (e: Exception) {
-                null
+
+        private fun getField(document: Document, field: String, defaultValue: String = ""): String {
+            return if (document.data.containsKey(field)) {
+                document.data[field] as String
+            } else {
+                ""
             }
+        }
+
+        fun fromDocument(document: Document, username: String): DashPayProfile? {
+
+            val displayName = getField(document, "displayName")
+            val publicMessage = getField(document, "publicMessage")
+            val avatarUrl = getField(document, "avatarUrl")
+
+            return DashPayProfile(document.ownerId,
+                    username,
+                    displayName,
+                    publicMessage,
+                    avatarUrl,
+                    if (document.createdAt != null) document.createdAt!! else 0L,
+                    if (document.updatedAt != null) document.updatedAt!! else 0L)
         }
     }
 }
