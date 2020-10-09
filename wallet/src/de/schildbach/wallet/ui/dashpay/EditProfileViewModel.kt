d73df75ca0394e296318272b5a3080ad5af2772f
Hash Engineering Solutions <hashengineeringsolutions@gmail.com>
2020-10-09 12:41:18 -0700

    DashPay: Add create/update profile support (#520)
    * Add create/update profile functionality to EditProfile and More
    
    * Add space in the character count string
    
    * PlatformRepo: Fix exceptions when making profile or name (where in) queries
    
    Co-authored-by: Tomasz Ludek <tomasz@dash.org>

---
+++ wallet/src/de/schildbach/wallet/ui/dashpay/EditProfileViewModel.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 Dash Core Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.schildbach.wallet.ui.dashpay
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import de.schildbach.wallet.AppDatabase
+import de.schildbach.wallet.WalletApplication
+import de.schildbach.wallet.data.BlockchainIdentityData
+import de.schildbach.wallet.data.DashPayProfile
+import de.schildbach.wallet.ui.dashpay.work.UpdateProfileOperation
+
+class EditProfileViewModel(application: Application) : AndroidViewModel(application) {
+    private val walletApplication = application as WalletApplication
+
+    // Use the database instead of PlatformRepo.getBlockchainIdentity, which
+    // won't be initialized if there is no username registered
+    val blockchainIdentityData = AppDatabase.getAppDatabase()
+            .blockchainIdentityDataDaoAsync().load()
+
+    val blockchainIdentity: BlockchainIdentityData?
+        get() = blockchainIdentityData.value
+
+    // this must be observed after blockchainIdentityData is observed
+    private lateinit var _dashPayProfileData: LiveData<DashPayProfile?>
+    val dashPayProfileData: LiveData<DashPayProfile?>
+        get() {
+            if (!this::_dashPayProfileData.isInitialized) {
+                _dashPayProfileData = AppDatabase.getAppDatabase()
+                        .dashPayProfileDaoAsync()
+                        .loadByUserIdDistinct(blockchainIdentity!!.userId!!)
+            }
+            return _dashPayProfileData
+        }
+
+    val updateProfileRequestState = UpdateProfileOperation.operationStatus(application)
+
+    fun broadcastUpdateProfile(dashPayProfile: DashPayProfile) {
+        UpdateProfileOperation(walletApplication)
+                .create(dashPayProfile)
+                .enqueue()
+    }
+}
---
+++ wallet/src/de/schildbach/wallet/ui/dashpay/work/UpdateProfileWorker.kt
@@ -0,0 +1,73 @@
+package de.schildbach.wallet.ui.dashpay.work
+
+import android.content.Context
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import de.schildbach.wallet.WalletApplication
+import de.schildbach.wallet.data.DashPayProfile
+import de.schildbach.wallet.ui.dashpay.PlatformRepo
+import de.schildbach.wallet.ui.security.SecurityGuard
+import org.bitcoinj.crypto.KeyCrypterException
+import org.bouncycastle.crypto.params.KeyParameter
+import java.io.IOException
+import java.security.GeneralSecurityException
+
+class UpdateProfileWorker(context: Context, parameters: WorkerParameters)
+    : BaseWorker(context, parameters) {
+
+    companion object {
+        const val KEY_PASSWORD = "UpdateProfileRequestWorker.PASSWORD"
+        const val KEY_DISPLAY_NAME = "UpdateProfileRequestWorker.DISPLAY_NAME"
+        const val KEY_PUBLIC_MESSAGE = "UpdateProfileRequestWorker.PUBLIC_MESSAGE"
+        const val KEY_AVATAR_URL = "UpdateProfileRequestWorker.AVATAR_URL"
+        const val KEY_USER_ID = "UpdateProfileRequestWorker.KEY_USER_ID"
+        const val KEY_CREATED_AT = "UpdateProfileRequestWorker.CREATED_AT"
+    }
+
+    private val platformRepo = PlatformRepo.getInstance()
+
+    override suspend fun doWork(): Result {
+        val displayName = inputData.getString(KEY_DISPLAY_NAME)?:""
+        val publicMessage = inputData.getString(KEY_PUBLIC_MESSAGE)?:""
+        val avatarUrl = inputData.getString(KEY_AVATAR_URL)?:""
+        if (!inputData.keyValueMap.containsKey(KEY_CREATED_AT))
+                return Result.failure(workDataOf(KEY_ERROR_MESSAGE to "missing KEY_CREATED_AT parameter"))
+        val createdAt = inputData.getLong(KEY_CREATED_AT, 0L)
+        val blockchainIdentity = platformRepo.getBlockchainIdentity()!!
+        val dashPayProfile = DashPayProfile(blockchainIdentity.uniqueIdString,
+                blockchainIdentity.getUniqueUsername(),
+                displayName,
+                publicMessage,
+                avatarUrl,
+                createdAt
+        )
+
+        val encryptionKey: KeyParameter
+        try {
+            val password = SecurityGuard().retrievePassword()
+            encryptionKey = WalletApplication.getInstance().wallet!!.keyCrypter!!.deriveKey(password)
+        } catch (ex: KeyCrypterException) {
+            val msg = formatExceptionMessage("derive encryption key", ex)
+            return Result.failure(workDataOf(KEY_ERROR_MESSAGE to msg))
+        } catch (ex: Exception) {
+            when (ex) {
+                is GeneralSecurityException,
+                is IOException -> {
+                    val msg = formatExceptionMessage("retrieve password", ex)
+                    return Result.failure(workDataOf(KEY_ERROR_MESSAGE to msg))
+                }
+                else -> throw ex
+            }
+        }
+
+        return try {
+            val profileRequestResult = platformRepo.broadcastUpdatedProfile(dashPayProfile!!, encryptionKey)
+            Result.success(workDataOf(
+                    KEY_USER_ID to profileRequestResult.userId
+            ))
+        } catch (ex: Exception) {
+            Result.failure(workDataOf(
+                    KEY_ERROR_MESSAGE to formatExceptionMessage("create/update profile", ex)))
+        }
+    }
+}
