296b8ccfe62c0c7dbf23753c13545c5a03112a6a
Tomasz Ludek <tomasz@dash.org>
2020-10-15 14:54:51 +0200

    Fixed the crash when taking picture on KitKat. Improved the way of storing profile picture in the app.

--- common/src/main/java/org/dash/wallet/common/Configuration.java
+++ common/src/main/java/org/dash/wallet/common/Configuration.java
@@ -78,7 +78,6 @@ public class Configuration {
     public static final String PREFS_V7_REDESIGN_TUTORIAL_COMPLETED = "v7_tutorial_completed";
     public static final String PREFS_PIN_LENGTH = "pin_length";
     public static final String PREFS_LAST_SEEN_NOTIFICATION_TIME = "last_seen_notification_time";
-    public static final String PREFS_KEY_LOCAL_PROFILE_PICTURE_URI = "local_profile_picture_uri";
 
     private static final int PREFS_DEFAULT_BTC_SHIFT = 0;
     private static final int PREFS_DEFAULT_BTC_PRECISION = 8;
@@ -434,13 +433,5 @@ public class Configuration {
 
     public void setLastSeenNotificationTime(long lastSeenNotificationTime) {
         prefs.edit().putLong(PREFS_LAST_SEEN_NOTIFICATION_TIME, lastSeenNotificationTime).apply();
-    }
-
-    public void setLocalProfilePictureUri(String picturePath) {
-        prefs.edit().putString(PREFS_KEY_LOCAL_PROFILE_PICTURE_URI, picturePath).apply();
-    }
-
-    public String getLocalProfilePictureUri() {
-        return prefs.getString(PREFS_KEY_LOCAL_PROFILE_PICTURE_URI, "");
     }
 }
