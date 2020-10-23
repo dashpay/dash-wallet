3ab784a1c5a0f7bb72a870893e22e82bb3257a7a
Hash Engineering Solutions <hashengineeringsolutions@gmail.com>
2020-10-23 12:48:49 -0700

    DashPay: Select Picture for Profile (#528)
    * Allow user to take a picture or select an existing picture
    
    * Handle Android 4.4 devices without crashing
    
    * PlatformRepo: Only update the local copy of a Profile if it was updated on Platform
    
    Co-authored-by: tomasz-ludek <tomasz@dash.org>

--- wallet/src/de/schildbach/wallet/ui/EditProfileActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/EditProfileActivity.kt
@@ -16,13 +16,24 @@
 
 package de.schildbach.wallet.ui
 
+import android.Manifest
+import android.content.ClipData
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
@@ -30,12 +41,25 @@ import de.schildbach.wallet.Constants
 import de.schildbach.wallet.data.DashPayProfile
 import de.schildbach.wallet.livedata.Status
 import de.schildbach.wallet.ui.dashpay.EditProfileViewModel
+import de.schildbach.wallet.ui.dashpay.SelectProfilePictureDialog
+import de.schildbach.wallet.ui.dashpay.SelectProfilePictureSharedViewModel
 import de.schildbach.wallet_test.R
 import kotlinx.android.synthetic.main.activity_edit_profile.*
+//import kotlinx.android.synthetic.main.activity_more.dashpayUserAvatar
+//import kotlinx.android.synthetic.main.activity_more.userInfoContainer
+import java.io.File
 
 class EditProfileActivity : BaseMenuActivity() {
 
+    companion object {
+        const val REQUEST_CODE_URI = 0
+        const val REQUEST_CODE_IMAGE = 1
+        const val REQUEST_CODE_CHOOSE_PICTURE_PERMISSION = 2
+        const val REQUEST_CODE_TAKE_PICTURE_PERMISSION = 3
+    }
+
     private lateinit var editProfileViewModel: EditProfileViewModel
+    private lateinit var selectProfilePictureSharedViewModel: SelectProfilePictureSharedViewModel
     private var isEditing: Boolean = false
 
     override fun getLayoutId(): Int {
@@ -105,10 +129,27 @@ class EditProfileActivity : BaseMenuActivity() {
         save.setOnClickListener {
             save()
         }
+
+        profile_edit_icon.setOnClickListener {
+            selectImage(this)
+        }
+
+        selectProfilePictureSharedViewModel.onTakePictureCallback.observe(this, Observer<Void> {
+            takePictureWithPermission()
+        })
+
+        selectProfilePictureSharedViewModel.onChoosePictureCallback.observe(this, Observer<Void> {
+            choosePictureWithPermission()
+        })
+
+        editProfileViewModel.onTmpPictureReadyForEditEvent.observe(this, Observer {
+            cropProfilePicture()
+        })
     }
 
     private fun initViewModel() {
         editProfileViewModel = ViewModelProvider(this).get(EditProfileViewModel::class.java)
+        selectProfilePictureSharedViewModel = ViewModelProvider(this).get(SelectProfilePictureSharedViewModel::class.java)
 
         // first ensure that we have a registered username
         editProfileViewModel.dashPayProfileData.observe(this, Observer { dashPayProfile ->
@@ -161,7 +202,11 @@ class EditProfileActivity : BaseMenuActivity() {
             Glide.with(dashpayUserAvatar).load(profile.avatarUrl).circleCrop()
                     .placeholder(defaultAvatar).into(dashpayUserAvatar)
         } else {
-            dashpayUserAvatar.setImageDrawable(defaultAvatar)
+            if (editProfileViewModel.profilePictureFile != null && editProfileViewModel.profilePictureFile!!.exists()) {
+                dashpayUserAvatar.setImageURI(getFileUri(editProfileViewModel.profilePictureFile!!))
+            } else {
+                dashpayUserAvatar.setImageDrawable(defaultAvatar)
+            }
         }
 
         about_me.setText(profile.publicMessage)
@@ -172,4 +217,115 @@ class EditProfileActivity : BaseMenuActivity() {
         this.isEditing = isEditing
     }
 
+    private fun takePictureWithPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePicture()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_TAKE_PICTURE_PERMISSION)
+        }
+    }
+
+    private fun choosePictureWithPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            choosePicture()
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_CHOOSE_PICTURE_PERMISSION)
+        }
+    }
+
+    private fun selectImage(context: Context) {
+        SelectProfilePictureDialog.createDialog()
+                .show(supportFragmentManager, "selectPictureDialog");
+    }
+
+    private fun choosePicture() {
+        if (editProfileViewModel.createTmpPictureFile()) {
+            val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(pickPhoto, REQUEST_CODE_URI)
+        } else {
+            Toast.makeText(this, "Unable to create temporary file", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun takePicture() {
+        if (editProfileViewModel.createTmpPictureFile()) {
+            dispatchTakePictureIntent()
+        } else {
+            Toast.makeText(this, "Unable to create temporary file", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val tmpFileUri = getFileUri(editProfileViewModel.tmpPictureFile)
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, tmpFileUri)
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP) {
+                    // to avoid 'SecurityException: Permission Denial' on KitKat
+                    takePictureIntent.clipData = ClipData.newRawUri("", tmpFileUri)
+                    takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                }
+                startActivityForResult(takePictureIntent, REQUEST_CODE_IMAGE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != RESULT_CANCELED) {
+            when (requestCode) {
+                REQUEST_CODE_IMAGE -> {
+                    if (resultCode == RESULT_OK) {
+                        // picture saved in editProfileViewModel.profilePictureTmpFile
+                        editProfileViewModel.onTmpPictureReadyForEditEvent.call(editProfileViewModel.tmpPictureFile)
+                    }
+                }
+                REQUEST_CODE_URI -> if (resultCode == RESULT_OK && data != null) {
+                    val selectedImage: Uri? = data.data
+                    val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+                    if (selectedImage != null) {
+                        val cursor: Cursor? = contentResolver.query(selectedImage,
+                                filePathColumn, null, null, null)
+                        if (cursor != null) {
+                            cursor.moveToFirst()
+                            val columnIndex: Int = cursor.getColumnIndex(filePathColumn[0])
+                            val picturePath: String = cursor.getString(columnIndex)
+                            editProfileViewModel.saveAsProfilePictureTmp(picturePath)
+                            cursor.close()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun cropProfilePicture() {
+        val tmpPictureFile = editProfileViewModel.tmpPictureFile
+        //TODO: this line is for debugging - show the selected image on the screen
+        dashpayUserAvatar.setImageURI(getFileUri(tmpPictureFile))
+        editProfileViewModel.saveTmpAsProfilePicture()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CODE_TAKE_PICTURE_PERMISSION -> {
+                when {
+                    grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED -> takePicture()
+                    else -> takePictureWithPermission()
+                }
+            }
+            REQUEST_CODE_CHOOSE_PICTURE_PERMISSION -> {
+                when {
+                    grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED -> choosePicture()
+                    else -> choosePictureWithPermission()
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    private fun getFileUri(file: File): Uri {
+        return FileProvider.getUriForFile(walletApplication, "${walletApplication.packageName}.file_attachment", file)
+    }
 }
--- wallet/src/de/schildbach/wallet/ui/dashpay/EditProfileViewModel.kt
+++ wallet/src/de/schildbach/wallet/ui/dashpay/EditProfileViewModel.kt
@@ -16,17 +16,56 @@
 package de.schildbach.wallet.ui.dashpay
 
 import android.app.Application
+import android.os.Environment
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.switchMap
 import de.schildbach.wallet.AppDatabase
+import de.schildbach.wallet.Constants
 import de.schildbach.wallet.WalletApplication
 import de.schildbach.wallet.data.DashPayProfile
+import de.schildbach.wallet.ui.SingleLiveEvent
 import de.schildbach.wallet.ui.dashpay.work.UpdateProfileOperation
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.channels.FileChannel
 import de.schildbach.wallet.ui.dashpay.work.UpdateProfileStatusLiveData
 
 class EditProfileViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val log = LoggerFactory.getLogger(EditProfileViewModel::class.java)
     private val walletApplication = application as WalletApplication
+
+    val profilePictureFile by lazy {
+        try {
+            val storageDir: File = application.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+            File(storageDir, Constants.Files.PROFILE_PICTURE_FILENAME)
+        } catch (ex: IOException) {
+            log.error(ex.message, ex)
+            null
+        }
+    }
+
+    val onTmpPictureReadyForEditEvent = SingleLiveEvent<File>()
+
+    lateinit var tmpPictureFile: File
+
+    fun createTmpPictureFile(): Boolean = try {
+        val storageDir: File = walletApplication.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        tmpPictureFile = File.createTempFile("profileimagetmp", ".jpg", storageDir).apply {
+            deleteOnExit()
+        }
+        true
+    } catch (ex: IOException) {
+        log.error(ex.message, ex)
+        false
+    }
 
     // blockchainIdentityData is observed instead of using PlatformRepo.getBlockchainIdentity()
     // since neither PlatformRepo nor blockchainIdentity is initialized when there is no username
@@ -53,5 +92,33 @@ class EditProfileViewModel(application: Application) : AndroidViewModel(applicat
         UpdateProfileOperation(walletApplication)
                 .create(updatedProfile)
                 .enqueue()
+    }
+
+    fun saveTmpAsProfilePicture() {
+        copyFile(tmpPictureFile, profilePictureFile!!)
+    }
+
+    fun saveAsProfilePictureTmp(picturePath: String) {
+        viewModelScope.launch() {
+            copyFile(File(picturePath), tmpPictureFile)
+            onTmpPictureReadyForEditEvent.call(tmpPictureFile)
+        }
+    }
+
+    private fun copyFile(srcFile: File, outFile: File) {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val inStream = FileInputStream(srcFile)
+                val outStream = FileOutputStream(outFile)
+                val inChannel: FileChannel = inStream.channel
+                val outChannel: FileChannel = outStream.channel
+                inChannel.transferTo(0, inChannel.size(), outChannel)
+                inStream.close()
+                outStream.close()
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
