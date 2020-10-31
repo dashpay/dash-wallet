bdbecaf13bf5d06e2bb74652997cf8b9bc42c5a2
HashEngineering <hashengineeringsolutions@gmail.com>
2020-10-30 18:12:10 -0700

    Add lazy fields for Identifiers and ByteArrays

--- wallet/src/de/schildbach/wallet/data/DashPayProfile.kt
+++ wallet/src/de/schildbach/wallet/data/DashPayProfile.kt
@@ -2,9 +2,11 @@ package de.schildbach.wallet.data
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import org.dashevo.dpp.document.Document
+import org.dashevo.dpp.identifier.Identifier
 
 @Parcelize
 @Entity(tableName = "dashpay_profile")
@@ -31,7 +33,7 @@ data class DashPayProfile(@PrimaryKey val userId: String,
             val publicMessage = getField(document, "publicMessage")
             val avatarUrl = getField(document, "avatarUrl")
 
-            return DashPayProfile(document.ownerId,
+            return DashPayProfile(document.ownerId.toString(),
                     username,
                     displayName,
                     publicMessage,
@@ -39,5 +41,14 @@ data class DashPayProfile(@PrimaryKey val userId: String,
                     if (document.createdAt != null) document.createdAt!! else 0L,
                     if (document.updatedAt != null) document.updatedAt!! else 0L)
         }
+    }
+
+    @delegate:Ignore
+    val userIdentifier by lazy {
+        Identifier.from(userId)
+    }
+    @delegate:Ignore
+    val rawUserId by lazy {
+        userIdentifier.toBuffer()
     }
 }
