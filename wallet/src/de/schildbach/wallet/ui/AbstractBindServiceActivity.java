2d71d7666a153e7186fa2e80eec3cd0b42c74180
Tomasz Ludek <tomasz@dash.org>
2020-11-30 18:30:37 +0100

    Fixed the class name in logger config

--- wallet/src/de/schildbach/wallet/ui/AbstractBindServiceActivity.java
+++ wallet/src/de/schildbach/wallet/ui/AbstractBindServiceActivity.java
@@ -36,7 +36,7 @@ import org.slf4j.LoggerFactory;
  */
 public abstract class AbstractBindServiceActivity extends AbstractWalletActivity {
 
-    private static final Logger log = LoggerFactory.getLogger(BlockchainServiceImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(AbstractBindServiceActivity.class);
 
     @Nullable
     private BlockchainService blockchainService;
