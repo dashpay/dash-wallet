dcdc571085a498ca476afbfe8146d3cfc89127c2
Tomasz Ludek <tomasz@dash.org>
2021-01-07 12:11:37 +0100

    Moved all the stuff from ui-redesign into common module. Added basic implementation of FancyAlertDialog.

---
+++ common/src/main/java/org/dash/wallet/common/ui/FancyAlertDialog.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 Dash Core Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dash.wallet.common.ui
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams
+import android.view.Window
+import androidx.fragment.app.DialogFragment
+import org.dash.wallet.common.R
+
+class FancyAlertDialog : DialogFragment() {
+
+    companion object {
+        fun newInstance(title: String?): FancyAlertDialog {
+            val args = Bundle().apply {
+                putString("title", title)
+            }
+            return FancyAlertDialog().apply {
+                arguments = args
+            }
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Set transparent background and no title
+        dialog?.window?.apply {
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            requestFeature(Window.FEATURE_NO_TITLE)
+        }
+        return inflater.inflate(R.layout.fancy_alert_dialog, container)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+    }
+}
