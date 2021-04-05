1c9a7c317b127f70c47097fb6dafa7a3e8a4db32
HashEngineering <hashengineeringsolutions@gmail.com>
2021-04-05 14:54:35 -0700

    NotificationsLiveData: Include user alerts only when there is enough balance or invites exist

--- wallet/src/de/schildbach/wallet/ui/dashpay/NotificationsLiveData.kt
+++ wallet/src/de/schildbach/wallet/ui/dashpay/NotificationsLiveData.kt
@@ -2,10 +2,7 @@ package de.schildbach.wallet.ui.dashpay
 
 import de.schildbach.wallet.AppDatabase
 import de.schildbach.wallet.WalletApplication
-import de.schildbach.wallet.data.NotificationItem
-import de.schildbach.wallet.data.NotificationItemUserAlert
-import de.schildbach.wallet.data.NotificationItemContact
-import de.schildbach.wallet.data.UsernameSortOrderBy
+import de.schildbach.wallet.data.*
 import de.schildbach.wallet.livedata.Resource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -27,7 +24,7 @@ open class NotificationsLiveData(walletApplication: WalletApplication,
             val results = arrayListOf<NotificationItem>()
 
             val userAlert = AppDatabase.getAppDatabase().userAlertDao().load(0L)
-            if (userAlert != null) {
+            if (userAlert != null && PlatformRepo.getInstance().shouldShowAlert()) {
                 results.add(NotificationItemUserAlert(userAlert.stringResId, userAlert.iconResId))
             }
 
