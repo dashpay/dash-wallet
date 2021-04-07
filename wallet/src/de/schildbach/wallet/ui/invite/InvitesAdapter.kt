4567f3d811973c9947bf63a7d35d6129e12e7ae6
HashEngineering <hashengineeringsolutions@gmail.com>
2021-04-07 09:10:16 -0700

    Add copyright to new files, format

--- wallet/src/de/schildbach/wallet/ui/invite/InvitesAdapter.kt
+++ wallet/src/de/schildbach/wallet/ui/invite/InvitesAdapter.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 Dash Core Group
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.schildbach.wallet.ui.invite
 
 import android.view.LayoutInflater
@@ -18,9 +34,6 @@ class InvitesAdapter(private val itemClickListener: OnItemClickListener,
         const val INVITE = 2
         const val EMPTY_HISTORY = 3
         const val INVITE_CREATE = 4
-        //val headerId: Sha256Hash = Sha256Hash.of(Sha256Hash.ZERO_HASH.bytes)
-        //val emptyId: Sha256Hash = Sha256Hash.of(headerId.bytes)
-        //val createId: Sha256Hash = Sha256Hash.of(BigInteger.valueOf(INVITE_CREATE.toLong()).toByteArray())
         val headerInvite = InvitationItem(INVITE_HEADER)
         val emptyInvite = InvitationItem(EMPTY_HISTORY)
         val createInvite = InvitationItem(INVITE_CREATE)
