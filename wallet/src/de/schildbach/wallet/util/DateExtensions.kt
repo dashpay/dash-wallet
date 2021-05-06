9ed5f3925ba9aa30b97cdb3feb447df55a6f89bd
HashEngineering <hashengineeringsolutions@gmail.com>
2021-05-06 11:58:02 -0700

    refactor: Use different method to rewind the time to the zero hour

--- wallet/src/de/schildbach/wallet/util/DateExtensions.kt
+++ wallet/src/de/schildbach/wallet/util/DateExtensions.kt
@@ -1,14 +1,15 @@
 package de.schildbach.wallet.util
 
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 fun Date.getDateAtHourZero(): Date {
-    val daysInMillis = TimeUnit.DAYS.toMillis(1)
-    val txDateCopy = Date(time)
-    val timeAtHourZero = time / daysInMillis * daysInMillis
-    txDateCopy.time = timeAtHourZero
-    return txDateCopy
+    val cal = Calendar.getInstance()
+    cal.time = this
+    cal[Calendar.HOUR_OF_DAY] = 0
+    cal[Calendar.MINUTE] = 0
+    cal[Calendar.SECOND] = 0
+    cal[Calendar.MILLISECOND] = 0
+    return cal.time
 }
 
 
