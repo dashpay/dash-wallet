b320b77c100ac2fe5c8a4665ac6730df12ed9df8
HashEngineering <hashengineeringsolutions@gmail.com>
2021-08-18 10:34:17 -0700

    fix: start the correct activity based on wallet existence

--- wallet/src/de/schildbach/wallet/ui/invite/InviteHandler.kt
+++ wallet/src/de/schildbach/wallet/ui/invite/InviteHandler.kt
@@ -63,7 +63,8 @@ class InviteHandler(val activity: AppCompatActivity) {
             activity.setResult(Activity.RESULT_CANCELED)
             val walletApplication = WalletApplication.getInstance()
             val mainTask = getMainTask(activity)
-            if (walletApplication.wallet == null) {
+            if (walletApplication.wallet != null) {
+                // if wallet exists, go to the Home Screen
                 mainTask.startActivity(activity.applicationContext, MainActivity.createIntent(activity), null)
             } else {
                 mainTask.startActivity(activity.applicationContext, OnboardingActivity.createIntent(activity), null)
