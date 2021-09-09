fa9a825658c2b7935caaf1cdbe397861587ad976
Andrei Ashikhmin <synmcj@gmail.com>
2021-09-09 23:48:06 +0700

    feat: add Explore Dash screen (#747)
    * [NMA-987] Portal | DashPay Alpha Program
    
    * feat: testnet alpha program explore screen
    
    * fix: sticky 'get test dash' button and small device adaptations

---
+++ wallet/src/de/schildbach/wallet/ui/explore/ExploreTestNetFragment.kt
@@ -0,0 +1,37 @@
+package de.schildbach.wallet.ui.explore
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
+import de.schildbach.wallet.Constants
+import de.schildbach.wallet.ui.dashpay.BottomNavFragment
+import de.schildbach.wallet_test.R
+import de.schildbach.wallet_test.databinding.FragmentExploreTestnetBinding
+import org.dash.wallet.common.ui.viewBinding
+
+@AndroidEntryPoint
+class ExploreTestNetFragment : BottomNavFragment(R.layout.fragment_explore_testnet) {
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): ExploreTestNetFragment {
+            return ExploreTestNetFragment()
+        }
+    }
+
+    override val navigationItemId = R.id.discover
+    private val binding by viewBinding(FragmentExploreTestnetBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.getDashBtn.setOnClickListener {
+            val faucetIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse(Constants.TESTNET_FAUCET_URL)
+            )
+            startActivity(faucetIntent)
+        }
+    }
+}
