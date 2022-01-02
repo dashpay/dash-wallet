10821a3c7f8cc5ca2ea6c90786aa4fa3598cf661
Andrei Ashikhmin <synmcj@gmail.com>
2022-01-03 04:27:15 +0700

    fix: getLockScreenViewModel and FancyAlertDialog crashes (#853)
    * fix: fix AndroidRuntimeException: requestFeature() must be called before adding content
    
    * fix: crash in getLockScreenViewModel
    
    Co-authored-by: HashEngineering <hashengineeringsolutions@gmail.com>

--- common/src/main/java/org/dash/wallet/common/ui/FancyAlertDialog.kt
+++ common/src/main/java/org/dash/wallet/common/ui/FancyAlertDialog.kt
@@ -30,14 +30,14 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
 import kotlinx.android.synthetic.main.fancy_alert_dialog.*
 import org.dash.wallet.common.R
 import org.dash.wallet.common.UserInteractionAwareCallback
 
 class FancyAlertDialog : DialogFragment() {
-    private lateinit var lockScreenViewModel: LockScreenViewModel
-    private lateinit var sharedViewModel: FancyAlertDialogViewModel
+    private val lockScreenViewModel by activityViewModels<LockScreenViewModel>()
+    private val sharedViewModel by activityViewModels<FancyAlertDialogViewModel>()
 
     enum class Type {
         INFO,
@@ -102,7 +102,6 @@ class FancyAlertDialog : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initViewModels()
         setOrHideIfEmpty(title, "title")
         setOrHideIfEmpty(message, "message")
         setOrHideIfEmpty(image, "image")
@@ -161,8 +160,6 @@ class FancyAlertDialog : DialogFragment() {
         dialog?.apply {
             window?.apply {
                 setLayout(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
-                setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                requestFeature(Window.FEATURE_NO_TITLE)
                 callback = UserInteractionAwareCallback(this.callback, requireActivity())
             }
             if (type == Type.PROGRESS) {
@@ -170,10 +167,5 @@ class FancyAlertDialog : DialogFragment() {
                 setCanceledOnTouchOutside(false)
             }
         }
-    }
-
-    private fun initViewModels() {
-        lockScreenViewModel = ViewModelProvider(this)[LockScreenViewModel::class.java]
-        sharedViewModel = ViewModelProvider(this)[FancyAlertDialogViewModel::class.java]
     }
 }
