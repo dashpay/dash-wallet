11b3d58ed2fff733d2014c491ce1f1d4bcd0ac62
synmcj <synmcj@gmail.com>
2022-01-07 17:21:31 +0700

    feat: displaying last explore dash sync times

--- wallet/src/de/schildbach/wallet/ui/AboutActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/AboutActivity.kt
@@ -20,21 +20,33 @@ import android.content.*
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
 import com.google.firebase.installations.FirebaseInstallations
+import dagger.hilt.android.AndroidEntryPoint
+import de.schildbach.wallet.ExploreSyncWorker
 import de.schildbach.wallet.WalletApplication
 import de.schildbach.wallet.util.Toast
 import de.schildbach.wallet_test.BuildConfig
 import de.schildbach.wallet_test.R
 import kotlinx.android.synthetic.main.activity_about.*
+import kotlinx.coroutines.launch
 import org.bitcoinj.core.VersionMessage
 import org.dash.wallet.common.services.analytics.AnalyticsConstants
-import org.dash.wallet.common.services.analytics.FirebaseAnalyticsServiceImpl
+import org.dash.wallet.common.services.analytics.AnalyticsService
+import org.dash.wallet.features.exploredash.repository.ExploreRepository
+import java.lang.Exception
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class AboutActivity : BaseMenuActivity() {
-    private val analytics = FirebaseAnalyticsServiceImpl.getInstance()
+    @Inject
+    lateinit var analytics: AnalyticsService
+    @Inject
+    lateinit var firebaseRepository: ExploreRepository
 
     override fun getLayoutId(): Int {
         return R.layout.activity_about
@@ -60,6 +72,7 @@ class AboutActivity : BaseMenuActivity() {
         }
 
         showFirebaseInstallationId()
+        showExploreDashSyncStatus()
     }
 
     private fun showFirebaseInstallationId() {
@@ -68,12 +81,31 @@ class AboutActivity : BaseMenuActivity() {
             if (task.isSuccessful) {
                 firebase_installation_id.text = task.result
             }
-            firebase_installation_id.setOnClickListener {
-                (getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).run {
-                    setPrimaryClip(ClipData.newPlainText("Firebase Installation ID", firebase_installation_id.text))
-                }
-                Toast(this@AboutActivity).toast("Copied")
+            firebase_installation_id.setCopyable("Firebase Installation ID")
+        }
+    }
+
+    private fun showExploreDashSyncStatus() {
+        lifecycleScope.launch {
+            val formatFlags = DateUtils.FORMAT_SHOW_DATE or DateUtils.FORMAT_ABBREV_MONTH or DateUtils.FORMAT_SHOW_TIME
+
+            val formattedUpdateTime = try {
+                val timestamp = firebaseRepository.getLastUpdate()
+                DateUtils.formatDateTime(applicationContext, timestamp, formatFlags)
+            } catch (ex: Exception) {
+                getString(R.string.about_last_explore_dash_update_error)
             }
+
+            val preferences = applicationContext.getSharedPreferences(ExploreSyncWorker.SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+            val lastSync = preferences.getLong(ExploreSyncWorker.PREFS_LAST_SYNC_KEY, 0)
+            val formattedSyncTime = if (lastSync == 0L) {
+                getString(R.string.about_last_explore_dash_sync_never)
+            } else {
+                DateUtils.formatDateTime(applicationContext, lastSync, formatFlags)
+            }
+
+            explore_dash_last_sync.text = getString(R.string.about_last_explore_dash_sync, formattedUpdateTime, formattedSyncTime)
+            explore_dash_last_sync.setCopyable("Explore Dash last sync")
         }
     }
 
@@ -103,4 +135,12 @@ class AboutActivity : BaseMenuActivity() {
         alertDialog.show()
     }
 
+    private fun TextView.setCopyable(label: String) {
+        this.setOnClickListener {
+            (getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).run {
+                setPrimaryClip(ClipData.newPlainText(label, this.text))
+            }
+            Toast(this@AboutActivity).toast("Copied")
+        }
+    }
 }
