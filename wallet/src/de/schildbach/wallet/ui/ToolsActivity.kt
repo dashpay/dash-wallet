8ee7f51aa424ea37086853f1aaa81b362ad5bb5a
HashEngineering <hashengineeringsolutions@gmail.com>
2022-04-20 08:52:08 -0700

    fix: update text and UI and add view model

--- wallet/src/de/schildbach/wallet/ui/ToolsActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/ToolsActivity.kt
@@ -16,32 +16,27 @@
 
 package de.schildbach.wallet.ui
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.core.os.bundleOf
 import dagger.hilt.android.AndroidEntryPoint
-import de.schildbach.wallet.Constants
 import de.schildbach.wallet.WalletApplication
 import de.schildbach.wallet.ui.send.SweepWalletActivity
 import de.schildbach.wallet.util.Qr
 import de.schildbach.wallet.util.Toast
 import de.schildbach.wallet_test.R
 import kotlinx.android.synthetic.main.activity_tools.*
-import org.bitcoinj.crypto.DeterministicKey
-import org.bitcoinj.uri.BitcoinURIParseException
 import org.dash.wallet.common.services.analytics.AnalyticsConstants
 import org.dash.wallet.common.services.analytics.AnalyticsService
 import org.dash.wallet.common.ui.BaseAlertDialogBuilder
 import org.dash.wallet.common.ui.dialogs.AdaptiveDialog
 import org.slf4j.LoggerFactory
-import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -53,6 +48,7 @@ class ToolsActivity : BaseMenuActivity() {
 
     @Inject
     lateinit var analytics: AnalyticsService
+    private val viewModel: ToolsViewModel by viewModels()
 
     override fun getLayoutId(): Int {
         return R.layout.activity_tools
@@ -79,23 +75,29 @@ class ToolsActivity : BaseMenuActivity() {
             handleExtendedPublicKey()
         }
 
+        var isSyncing = false
+        viewModel.blockchainState.observe(this) {
+            isSyncing = it?.replaying == true
+        }
+
         transaction_export.setOnClickListener {
-            alertDialog = ExportTransactionHistoryDialogBuilder.createExportTransactionDialog(this,
-                WalletApplication.getInstance()).buildAlertDialog()
-            alertDialog.show()
+            if (isSyncing) {
+                val dialog = AdaptiveDialog.create(
+                    null,
+                    getString(R.string.report_transaction_history_not_synced_title),
+                    getString(R.string.report_transaction_history_not_synced_message),
+                    getString(R.string.close))
+                dialog.show(supportFragmentManager, "requireSyncing")
+            } else {
+                alertDialog = ExportTransactionHistoryDialogBuilder.createExportTransactionDialog(this,
+                    WalletApplication.getInstance()).buildAlertDialog()
+                alertDialog.show()
+            }
         }
     }
 
     private fun handleExtendedPublicKey() {
-        val extendedKey: DeterministicKey = walletApplication.wallet.watchingKey
-        val xpub = extendedKey.serializePubB58(Constants.NETWORK_PARAMETERS);
-        val xpubWithCreationDate = String.format(
-            Locale.US,
-            "%s?c=%d&h=bip44",
-            xpub,
-            extendedKey.creationTimeSeconds
-        )
-        showExtendedPublicKeyDialog(xpubWithCreationDate, xpub)
+        showExtendedPublicKeyDialog(viewModel.xpubWithCreationDate, viewModel.xpub)
     }
 
     private fun showExtendedPublicKeyDialog(xpubWithCreationDate: String, xpub: String) {
@@ -145,8 +147,7 @@ class ToolsActivity : BaseMenuActivity() {
     }
 
     private fun handleCopyAddress(xpub: String) {
-        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipboardManager.setPrimaryClip(ClipData.newPlainText("Dash extendeded public key", xpub))
+        viewModel.copyXpubToClipboard()
 
         Toast(this).toast(R.string.receive_copied)
         log.info("xpub copied to clipboard: {}", xpub)
