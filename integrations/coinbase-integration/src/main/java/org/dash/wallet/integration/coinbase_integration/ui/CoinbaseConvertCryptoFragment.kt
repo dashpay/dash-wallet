3a6f144c7b78aac2767615226edf7ac96e7f18e1
hadia <hadiamohamed.iti@gmail.com>
2022-04-21 15:37:47 +0200

    fix : Add MIN_SWAP_DASH_AMOUNT_VALUE constant

--- integrations/coinbase-integration/src/main/java/org/dash/wallet/integration/coinbase_integration/ui/CoinbaseConvertCryptoFragment.kt
+++ integrations/coinbase-integration/src/main/java/org/dash/wallet/integration/coinbase_integration/ui/CoinbaseConvertCryptoFragment.kt
@@ -290,18 +290,6 @@ class CoinbaseConvertCryptoFragment : Fragment(R.layout.fragment_coinbase_conver
         when (swapValueErrorType) {
             SwapValueErrorType.LessThanMin -> setMinAmountErrorMessage()
             SwapValueErrorType.MoreThanMax -> setMaxAmountError()
-            SwapValueErrorType.UnAuthorizedValue ->
-
-                binding.authLimitBanner.warningLimitInfo.setOnClickListener {
-                    AdaptiveDialog.custom(
-                        R.layout.dialog_withdrawal_limit_info,
-                        null,
-                        getString(R.string.set_auth_limit),
-                        getString(R.string.change_withdrawal_limit),
-                        "",
-                        getString(R.string.got_it)
-                    ).show(requireActivity()) { }
-                }
         }
     }
 
