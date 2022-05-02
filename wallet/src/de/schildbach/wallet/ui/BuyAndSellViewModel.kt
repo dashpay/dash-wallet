6d22fa2ace1cf19c0732cbb0fb138dfa82cb6fb9
CLAUDE HANGUI <claude.hangui@gmail.com>
2022-05-02 21:58:24 +0000

    provide default value for coinbase balance when no value is available

--- wallet/src/de/schildbach/wallet/ui/BuyAndSellViewModel.kt
+++ wallet/src/de/schildbach/wallet/ui/BuyAndSellViewModel.kt
@@ -193,7 +193,10 @@ class BuyAndSellViewModel
                     _coinbaseBalance.value = response.value?.balance?.amount
                 }
                 is ResponseResource.Failure -> {
-                    _coinbaseBalance.value = config.lastCoinbaseBalance
+                    _coinbaseBalance.value =  if (config.lastCoinbaseBalance.isNullOrEmpty()) {
+                            config.lastCoinbaseBalance
+                        } else "0.0"
+                    }
                 }
             }
         }
