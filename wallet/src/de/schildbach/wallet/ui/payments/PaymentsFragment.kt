e1f0e6b34ed098ce4b2260e59eaf857841a99c42
synmcj <synmcj@gmail.com>
2022-09-16 21:08:08 +0700

    feat: transitions

--- wallet/src/de/schildbach/wallet/ui/payments/PaymentsFragment.kt
+++ wallet/src/de/schildbach/wallet/ui/payments/PaymentsFragment.kt
@@ -19,8 +19,15 @@ package de.schildbach.wallet.ui.payments
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.DecelerateInterpolator
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.transition.Fade
+import androidx.transition.Slide
+import androidx.transition.TransitionSet
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.transition.MaterialFadeThrough
@@ -37,7 +44,7 @@ class PaymentsFragment : Fragment(R.layout.fragment_payments) {
         private const val PREFS_RECENT_TAB = "recent_tab"
         private const val EXTRA_ACTIVE_TAB = "active_tab"
 
-        const val ACTIVE_TAB_RECEIVE = 0 // TODO: should be used
+        const val ACTIVE_TAB_RECEIVE = 0
         const val ACTIVE_TAB_PAY = 1
 
         @JvmStatic
@@ -56,7 +63,17 @@ class PaymentsFragment : Fragment(R.layout.fragment_payments) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        enterTransition = MaterialFadeThrough()
+        enterTransition = TransitionSet().apply {
+            duration = 200
+            addTransition(Slide())
+            addTransition(Fade().apply { interpolator = AccelerateInterpolator() })
+        }
+        returnTransition = MaterialFadeThrough()
+
+        binding.closeButton.setOnClickListener {
+            binding.closeButton.isVisible = false
+            findNavController().popBackStack()
+        }
 
         binding.tabs.provideOptions(listOf(
             SegmentedOption(getString(R.string.payments_tab_receive_label), R.drawable.ic_arrow_down),
@@ -72,7 +89,7 @@ class PaymentsFragment : Fragment(R.layout.fragment_payments) {
 
             override fun createFragment(position: Int): Fragment {
                 val fragment = when (position) {
-                    0 -> PaymentsReceiveFragment.newInstance()
+                    ACTIVE_TAB_RECEIVE -> PaymentsReceiveFragment.newInstance()
                     else -> PaymentsPayFragment.newInstance()
                 }
                 return fragment
@@ -97,10 +114,10 @@ class PaymentsFragment : Fragment(R.layout.fragment_payments) {
 
     private fun activateTab() {
         val activeTab = if (arguments?.containsKey(EXTRA_ACTIVE_TAB) == true) {
-            requireArguments().getInt(EXTRA_ACTIVE_TAB, 0)
+            requireArguments().getInt(EXTRA_ACTIVE_TAB, ACTIVE_TAB_RECEIVE)
         } else {
             val preferences = requireActivity().getPreferences(Context.MODE_PRIVATE)
-            preferences.getInt(PREFS_RECENT_TAB, 0)
+            preferences.getInt(PREFS_RECENT_TAB, ACTIVE_TAB_RECEIVE)
         }
 
         binding.tabs.setSelectedIndex(activeTab, false)
