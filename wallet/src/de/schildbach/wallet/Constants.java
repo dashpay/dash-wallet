2e2bd140773acb1632379204d36e7dd4359e982e
HashEngineering <hashengineeringsolutions@gmail.com>
2022-12-06 20:55:42 -0800

    fix: update comments for fee changes

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -317,9 +317,9 @@ public final class Constants {
     //Backup Warnings (true = both seed and backup file, false = seed only)
     public static final boolean SUPPORT_BOTH_BACKUP_WARNINGS = false;
 
-    // 100,000,000 credits
+    // 1,500,000,000 credits
     public static final Coin DASH_PAY_FEE = Coin.parseCoin("0.15");
-    //  10,000,000
+    // 150,000,000
     public static final Coin DASH_PAY_INVITE_MIN = DASH_PAY_FEE.div(10);
 
     public static boolean IS_TESTNET_BUILD = Constants.NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_TESTNET);
