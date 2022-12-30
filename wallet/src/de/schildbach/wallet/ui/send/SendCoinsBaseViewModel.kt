dfaf88638ca92520b68b6f18c567a2b2c76cb8e4
Andrei Ashikhmin <synmcj>
2022-12-30 15:09:23 +0700

    fix: ignore address assignment for intent with no address

--- wallet/src/de/schildbach/wallet/ui/send/SendCoinsBaseViewModel.kt
+++ wallet/src/de/schildbach/wallet/ui/send/SendCoinsBaseViewModel.kt
@@ -47,7 +47,10 @@ open class SendCoinsBaseViewModel @Inject constructor(
 
     open fun initPaymentIntent(paymentIntent: PaymentIntent) {
         basePaymentIntent = paymentIntent
-        _address.value = paymentIntent.address.toBase58()
+
+        if (paymentIntent.hasAddress()) { // avoid the exception for a missing address in a BIP70 payment request
+            _address.value = paymentIntent.address.toBase58()
+        }
     }
 
     protected fun createSendRequest(
