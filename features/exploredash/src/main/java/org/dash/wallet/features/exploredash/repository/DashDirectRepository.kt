7ca783d76a2a4ddca757defa763810e72ef743af
hadia <hadiamohamed.iti@gmail.com>
2023-01-24 21:44:10 +0200

    feat : fix getMerchantById

--- features/exploredash/src/main/java/org/dash/wallet/features/exploredash/repository/DashDirectRepository.kt
+++ features/exploredash/src/main/java/org/dash/wallet/features/exploredash/repository/DashDirectRepository.kt
@@ -118,17 +118,15 @@ class DashDirectRepository @Inject constructor(
     }
 
     override suspend fun getMerchantById(
-        deviceID: String,
         userEmail: String,
         merchantId: Long,
         includeLocations: Boolean?
-    )= safeApiCall {
+    ) = safeApiCall {
         servicesApi.getMerchantById(
-            deviceID = deviceID,
             email = userEmail,
             getDataMerchantIdRequest = GetDataMerchantIdRequest(
                 id = merchantId,
-                includeLocations= includeLocations
+                includeLocations = includeLocations
             )
         )
     }
@@ -142,6 +140,6 @@ interface DashDirectRepositoryInt {
     suspend fun logout()
     suspend fun purchaseGiftCard(deviceID: String, currency: String, giftCardAmount: Double, merchantId: Long, userEmail: String):
         ResponseResource<PurchaseGiftCardResponse?>
-    suspend fun getMerchantById(deviceID: String, userEmail: String,merchantId: Long, includeLocations: Boolean? = false):
-            ResponseResource<GetDataMerchantIdResponse?>
+    suspend fun getMerchantById(userEmail: String, merchantId: Long, includeLocations: Boolean? = false):
+        ResponseResource<GetDataMerchantIdResponse?>
 }
