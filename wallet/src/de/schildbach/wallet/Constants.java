b7d055bb00679b0fc2d4fd9c0ccd598e460c81b8
Hash Engineering Solutions <hashengineeringsolutions@gmail.com>
2023-02-18 12:53:33 -0800

    feat:  support core 19 hpmn features (#1087)
    * fix: use white-russian devnet
    
    * fix: explore db path

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -29,9 +29,8 @@ import org.bitcoinj.core.Context;
 import org.bitcoinj.core.MasternodeSync;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.crypto.ChildNumber;
-import org.bitcoinj.params.ChaChaDevNetParams;
-import org.bitcoinj.params.JackDanielsDevNetParams;
 import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.params.WhiteRussianDevNetParams;
 import org.bitcoinj.params.TestNet3Params;
 import org.bitcoinj.utils.MonetaryFormat;
 import org.bitcoinj.wallet.DeterministicKeyChain;
@@ -94,13 +93,13 @@ public final class Constants {
             case "devnet": {
                 // Devnet
                 BIP44_PATH = DeterministicKeyChain.BIP44_ACCOUNT_ZERO_PATH_TESTNET;
-                NETWORK_PARAMETERS = ChaChaDevNetParams.get();
+                NETWORK_PARAMETERS = WhiteRussianDevNetParams.get();
                 // TODO: remove this next line when Platform Supports Core 0.18
                 DNS_SEED = NETWORK_PARAMETERS.getDnsSeeds();
                 IS_PROD_BUILD = false;
                 FILENAME_NETWORK_SUFFIX = "-chacha";
                 WALLET_NAME_CURRENCY_CODE = "tdash";
-                org.dash.wallet.common.Constants.EXPLORE_GC_FILE_PATH = "explore/explore-testnet.db";
+                org.dash.wallet.common.util.Constants.EXPLORE_GC_FILE_PATH = "explore/explore-testnet.db";
                 SYNC_FLAGS.add(MasternodeSync.SYNC_FLAGS.SYNC_HEADERS_MN_LIST_FIRST);
                 break;
             }
