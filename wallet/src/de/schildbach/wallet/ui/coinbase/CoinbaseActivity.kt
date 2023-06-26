6d70d651d4a38df23aa026e4c10e7ba93c803d6c
Hadia <hadiamohamed.iti@gmail.com>
2023-06-26 18:28:57 +0300

    Fix(Coinbase): Expired token message (#1144)
    * fix :  Revoke token api
    
    * fix :  logout when token is expired
    
    * fix :  initial value equal zero for fait
    
    * fix : Remove unused code
    
    * fix : add Mutex  For multiple call to refresh token sync
    
    * feat : add refresh token dialog for coinbase
    
    * feat : add Dialog strings.xml
    
    * fix : when the session expired and we relogin again we get this dialog too
    
    
    ---------
    
    Co-authored-by: Hadia Ismail <hadia_ismail@kickstarter.com>

--- wallet/src/de/schildbach/wallet/ui/coinbase/CoinbaseActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/coinbase/CoinbaseActivity.kt
@@ -17,14 +17,19 @@
 
 package de.schildbach.wallet.ui.coinbase
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import de.schildbach.wallet.ui.BaseMenuActivity
 import de.schildbach.wallet_test.R
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.dash.wallet.common.ui.dialogs.AdaptiveDialog
 import org.dash.wallet.integration.coinbase_integration.viewmodels.CoinbaseActivityViewModel
 
 @ExperimentalCoroutinesApi
@@ -37,13 +42,86 @@ class CoinbaseActivity : BaseMenuActivity() {
         return R.layout.activity_coinbase
     }
 
+    private val coinbaseAuthLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        val data = result.data
+
+        if (result.resultCode == Activity.RESULT_OK) {
+            data?.extras?.getString(CoinBaseWebClientActivity.RESULT_TEXT)?.let { code ->
+                handleCoinbaseAuthResult(code)
+            }
+        }
+    }
+
+    private fun handleCoinbaseAuthResult(code: String) {
+        lifecycleScope.launchWhenResumed {
+            val success = AdaptiveDialog.withProgress(getString(R.string.loading), this@CoinbaseActivity) {
+                viewModel.loginToCoinbase(code)
+            }
+
+            if (success) {
+                val intent = intent
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                finish()
+                overridePendingTransition(0, 0)
+                startActivity(intent)
+                overridePendingTransition(0, 0)
+            } else {
+                AdaptiveDialog.create(
+                    R.drawable.ic_error,
+                    getString(R.string.login_error_title, getString(R.string.coinbase)),
+                    getString(R.string.login_error_message, getString(R.string.coinbase)),
+                    getString(android.R.string.cancel),
+                    getString(R.string.retry)
+                ).show(this@CoinbaseActivity) { retry ->
+                    if (retry == true) {
+                        handleCoinbaseAuthResult(code)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun continueCoinbase() {
+        coinbaseAuthLauncher.launch(
+            Intent(
+                this,
+                CoinBaseWebClientActivity::class.java
+            )
+        )
+    }
+
+    private val reLoginDialog: AdaptiveDialog by lazy {
+        AdaptiveDialog.create(
+            R.drawable.ic_relogin,
+            getString(R.string.your_coinbase_session_has_expired),
+            getString(R.string.please_log_in_to_your_coinbase_account),
+            getString(R.string.cancel),
+            getString(R.string.log_in)
+        ).also {
+            it.isCancelable = false
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         navController = setNavigationGraph()
 
         viewModel.coinbaseLogOutCallback.observe(this) {
-            finish()
+            if (reLoginDialog.isVisible) {
+                reLoginDialog.dismiss()
+            }
+            if (it == true) {
+                reLoginDialog.show(this) { login ->
+                    if (login == true) {
+                        continueCoinbase()
+                    } else {
+                        finish()
+                    }
+                }
+            }
         }
 
         viewModel.getPaymentMethods()
