130854511afe4c02fd9570e501aa60537419182d
Andrei Ashikhmin <synmcj@gmail.com>
2023-07-14 14:45:01 +0700

    feat: voting entrypoint

--- wallet/src/de/schildbach/wallet/ui/username/UsernameRequestsFragment.kt
+++ wallet/src/de/schildbach/wallet/ui/username/UsernameRequestsFragment.kt
@@ -17,8 +17,38 @@
 
 package de.schildbach.wallet.ui.username
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
 import de.schildbach.wallet_test.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.dash.wallet.common.ui.dialogs.AdaptiveDialog
+import org.dash.wallet.common.util.observe
 
+@AndroidEntryPoint
 class UsernameRequestsFragment : Fragment(R.layout.fragment_username_requests) {
+    private val viewModel: UsernameRequestsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.uiState.observe(viewLifecycleOwner) { state ->
+            if (state.showFirstTimeInfo) {
+                lifecycleScope.launch {
+                    delay(200)
+                    AdaptiveDialog.create(
+                        R.drawable.ic_user_list,
+                        getString(R.string.voting_duplicates_only_title),
+                        getString(R.string.voting_duplicates_only_message),
+                        getString(R.string.button_ok)
+                    ).showAsync(requireActivity())
+                    viewModel.setFirstTimeInfoShown()
+                }
+            }
+        }
+    }
 }
