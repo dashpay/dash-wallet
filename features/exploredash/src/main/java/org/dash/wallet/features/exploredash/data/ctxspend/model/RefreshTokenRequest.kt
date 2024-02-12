792b68147e3cc4848a72c083ad8dfa080e155495
HashEngineering <hashengineeringsolutions@gmail.com>
2024-02-12 13:12:05 -0800

    style: ktlint

--- features/exploredash/src/main/java/org/dash/wallet/features/exploredash/data/ctxspend/model/RefreshTokenRequest.kt
+++ features/exploredash/src/main/java/org/dash/wallet/features/exploredash/data/ctxspend/model/RefreshTokenRequest.kt
@@ -3,5 +3,5 @@ package org.dash.wallet.features.exploredash.data.ctxspend.model
 import com.google.gson.annotations.SerializedName
 
 data class RefreshTokenRequest(
-    @SerializedName("refreshToken") val refreshToken: String? = null,
+    @SerializedName("refreshToken") val refreshToken: String? = null
 )
