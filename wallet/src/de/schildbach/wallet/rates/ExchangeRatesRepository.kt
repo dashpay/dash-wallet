4ef1b81984840d359b973a6501350dc0d6f1b4dc
HashEngineering <hashengineeringsolutions@gmail.com>
2024-05-08 06:21:15 -0700

    feat: warn user of stale exchange rates (#1284)
    * feat: support detection of stale rates
    
    * feat: add new Toast based on Compose

--- wallet/src/de/schildbach/wallet/rates/ExchangeRatesRepository.kt
+++ wallet/src/de/schildbach/wallet/rates/ExchangeRatesRepository.kt
@@ -4,12 +4,23 @@ import androidx.lifecycle.MutableLiveData
 import de.schildbach.wallet.database.dao.ExchangeRatesDao
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.launch
 import org.dash.wallet.common.data.CurrencyInfo
+import org.dash.wallet.common.data.ExchangeRatesConfig
+import org.dash.wallet.common.data.ExchangeRatesConfig.Companion.EXCHANGE_RATES_PREVIOUS_RETRIEVAL_TIME
+import org.dash.wallet.common.data.ExchangeRatesConfig.Companion.EXCHANGE_RATES_RETRIEVAL_FAILURE
+import org.dash.wallet.common.data.ExchangeRatesConfig.Companion.EXCHANGE_RATES_RETRIEVAL_TIME
 import org.dash.wallet.common.data.entity.ExchangeRate
 import org.dash.wallet.common.services.ExchangeRatesProvider
+import org.dash.wallet.common.services.RateRetrievalState
 import org.slf4j.LoggerFactory
+import java.math.BigDecimal
 import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -18,11 +29,13 @@ import javax.inject.Inject
  * @author Samuel Barbosa
  */
 class ExchangeRatesRepository @Inject constructor(
-    private val exchangeRatesDao: ExchangeRatesDao
+    private val exchangeRatesDao: ExchangeRatesDao,
+    private val config: ExchangeRatesConfig
 ): ExchangeRatesProvider {
     companion object {
         private val log = LoggerFactory.getLogger(ExchangeRatesRepository::class.java)
         private val UPDATE_FREQ_MS = TimeUnit.SECONDS.toMillis(30)
+        private val STALE_DURATION_MS = TimeUnit.MINUTES.toMillis(30)
     }
 
     private val exchangeRatesClients: Deque<ExchangeRatesClient> = ArrayDeque()
@@ -34,6 +47,9 @@ class ExchangeRatesRepository @Inject constructor(
     var hasError = MutableLiveData<Boolean>()
     private var isRefreshing = false
     private val refreshScope = CoroutineScope(Dispatchers.IO)
+    private val updateTrigger = MutableSharedFlow<Long>()
+    // used to detect 50% change in rates since the last rates (volatile)
+    private val previousRates = arrayListOf<ExchangeRate>()
 
     init {
         populateExchangeRatesStack()
@@ -71,10 +87,16 @@ class ExchangeRatesRepository @Inject constructor(
                 }
 
                 if (rates.isNotEmpty()) {
+                    previousRates.clear()
+                    previousRates.addAll(exchangeRatesDao.getAll())
                     exchangeRatesDao.insertAll(rates)
                     lastUpdated = System.currentTimeMillis()
                     populateExchangeRatesStack()
                     hasError.postValue(false)
+                    config.set(EXCHANGE_RATES_RETRIEVAL_FAILURE, false)
+                    val prevRetrievalTime = config.get(EXCHANGE_RATES_RETRIEVAL_TIME) ?: 0
+                    config.set(EXCHANGE_RATES_PREVIOUS_RETRIEVAL_TIME, prevRetrievalTime)
+                    config.set(EXCHANGE_RATES_RETRIEVAL_TIME, System.currentTimeMillis())
                     isRefreshing = false
                     log.info("exchange rates updated successfully with {}", exchangeRatesClient)
                 } else if (!exchangeRatesClients.isEmpty()) {
@@ -91,12 +113,15 @@ class ExchangeRatesRepository @Inject constructor(
                 }
             } finally {
                 isLoading.postValue(false)
+                updateTrigger.emit(System.currentTimeMillis())
+                log.info("updateTrigger")
             }
         }
     }
 
     private suspend fun handleRefreshError() {
         isRefreshing = false
+        config.set(EXCHANGE_RATES_RETRIEVAL_FAILURE, true)
         if (exchangeRatesDao.count() == 0) {
             hasError.postValue(true)
         }
@@ -128,4 +153,52 @@ class ExchangeRatesRepository @Inject constructor(
         }
         return exchangeRatesDao.observeRate(currencyCode)
     }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun observeStaleRates(currencyCode: String): Flow<RateRetrievalState> = updateTrigger
+        .mapLatest {
+            val currentTime = System.currentTimeMillis()
+            val lastRetrievalTime = config.get(EXCHANGE_RATES_RETRIEVAL_TIME) ?: System.currentTimeMillis()
+
+            // are rates older than 30 minutes?
+            val staleRate = if ((currentTime - lastRetrievalTime) > STALE_DURATION_MS) {
+                true
+            } else {
+                val lastRate = exchangeRatesDao.getExchangeRateForCurrency(currencyCode)
+                lastRate != null && lastRate.retrievalTime != -1L &&
+                        (currentTime - lastRate.retrievalTime) > STALE_DURATION_MS
+            }
+            val previousRetrievalTime = config.get(EXCHANGE_RATES_PREVIOUS_RETRIEVAL_TIME) ?: 0
+            // have rates changed by 50% since the last values?
+            val volatile = if (lastRetrievalTime - previousRetrievalTime < TimeUnit.DAYS.toMillis(7)) {
+                val previousRate = previousRates.find { it.currencyCode == currencyCode }
+                previousRate?.let { prev ->
+                    prev.rate?.let { prevRate ->
+                        val oldRate = prevRate.toBigDecimal()
+                        exchangeRatesDao.getExchangeRateForCurrency(currencyCode)?.let { new ->
+                            new.rate?.let { newRate ->
+                                (newRate.toBigDecimal() - oldRate) / oldRate > BigDecimal(0.50)
+                            }
+                        }
+                    }
+                } ?: false
+            } else {
+                false
+            }
+            val retrievalError = config.get(EXCHANGE_RATES_RETRIEVAL_FAILURE) ?: false
+            RateRetrievalState(
+                retrievalError,
+                staleRate,
+                if (retrievalError || staleRate) false else volatile
+            )
+        }
+        .catch {
+            log.error("updateTrigger exception caught:", it)
+            RateRetrievalState(
+                false,
+                false,
+                false
+            )
+        }
+        .distinctUntilChanged()
 }
--- wallet/src/de/schildbach/wallet/ui/main/MainViewModel.kt
+++ wallet/src/de/schildbach/wallet/ui/main/MainViewModel.kt
@@ -44,6 +44,7 @@ import org.dash.wallet.common.data.entity.BlockchainState
 import org.dash.wallet.common.data.entity.ExchangeRate
 import org.dash.wallet.common.services.BlockchainStateProvider
 import org.dash.wallet.common.services.ExchangeRatesProvider
+import org.dash.wallet.common.services.RateRetrievalState
 import org.dash.wallet.common.services.TransactionMetadataProvider
 import org.dash.wallet.common.services.analytics.AnalyticsConstants
 import org.dash.wallet.common.services.analytics.AnalyticsService
@@ -113,6 +114,19 @@ class MainViewModel @Inject constructor(
     val exchangeRate: LiveData<ExchangeRate>
         get() = _exchangeRate
 
+    private val _rateStale = MutableStateFlow(
+        RateRetrievalState(
+            lastAttemptFailed = false,
+            staleRates = false,
+            volatile = false
+        )
+    )
+    val rateStale: Flow<RateRetrievalState>
+        get() = _rateStale
+    val currentStaleRateState
+        get() = _rateStale.value
+    var rateStaleDismissed = false
+
     private val _balance = MutableLiveData<Coin>()
     val balance: LiveData<Coin>
         get() = _balance
@@ -180,6 +194,15 @@ class MainViewModel @Inject constructor(
                     .filterNotNull()
             }
             .onEach(_exchangeRate::postValue)
+            .launchIn(viewModelScope)
+
+        walletUIConfig
+            .observe(WalletUIConfig.SELECTED_CURRENCY)
+            .filterNotNull()
+            .flatMapLatest { code ->
+                exchangeRatesProvider.observeStaleRates(code)
+            }
+            .onEach(_rateStale::emit)
             .launchIn(viewModelScope)
     }
 
