c751326e026dfb7952130e823127993e654f5336
HashEngineering <hashengineeringsolutions@gmail.com>
2024-08-15 11:56:11 -0700

    feat: move balance requirement to WelcomeToDashPayFragment

--- wallet/src/de/schildbach/wallet/ui/username/voting/WelcomeToDashPayFragment.kt
+++ wallet/src/de/schildbach/wallet/ui/username/voting/WelcomeToDashPayFragment.kt
@@ -2,8 +2,11 @@ package de.schildbach.wallet.ui.username.voting
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import dagger.hilt.android.AndroidEntryPoint
+import de.schildbach.wallet.Constants
 import de.schildbach.wallet_test.R
 import de.schildbach.wallet_test.databinding.FragmentWelcomeToDashpayBinding
 import org.dash.wallet.common.ui.viewBinding
@@ -12,7 +15,7 @@ import org.dash.wallet.common.util.safeNavigate
 @AndroidEntryPoint
 class WelcomeToDashPayFragment : Fragment(R.layout.fragment_welcome_to_dashpay) {
     private val binding by viewBinding(FragmentWelcomeToDashpayBinding::bind)
-
+    private val requestUserNameViewModel by activityViewModels<RequestUserNameViewModel>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -21,5 +24,13 @@ class WelcomeToDashPayFragment : Fragment(R.layout.fragment_welcome_to_dashpay) 
         binding.welcomeDashpayContinueBtn.setOnClickListener {
             safeNavigate(WelcomeToDashPayFragmentDirections.welcomeToDashPayFragmentToUsernameVotingInfoFragment())
         }
+
+        binding.balanceRequirementDisclaimer.text = getString(
+            R.string.welcome_request_username_min_balance_disclaimer,
+            requestUserNameViewModel.walletBalance.toPlainString(),
+            Constants.DASH_PAY_FEE_CONTESTED.toPlainString()
+        )
+        binding.balanceRequirementDisclaimer.isVisible = !requestUserNameViewModel.canAffordIdentityCreation()
+        binding.welcomeDashpayContinueBtn.isEnabled = requestUserNameViewModel.canAffordIdentityCreation()
     }
 }
