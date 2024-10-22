1e54a62de207dcfc69eea58a393dc88868c4d566
HashEngineering <hashengineeringsolutions@gmail.com>
2024-10-22 08:20:43 -0700

    fix: remove unnecessary firebase id in SendCoinsTaskRunner

--- wallet/src/de/schildbach/wallet/payments/SendCoinsTaskRunner.kt
+++ wallet/src/de/schildbach/wallet/payments/SendCoinsTaskRunner.kt
@@ -17,7 +17,6 @@
 package de.schildbach.wallet.payments
 
 import androidx.annotation.VisibleForTesting
-import com.google.firebase.installations.FirebaseInstallations
 import de.schildbach.wallet.WalletApplication
 import de.schildbach.wallet.data.CoinJoinConfig
 import de.schildbach.wallet.data.PaymentIntent
@@ -88,10 +87,6 @@ class SendCoinsTaskRunner @Inject constructor(
                 coinJoinSend = mode != CoinJoinMode.NONE
             }
             .launchIn(coroutineScope)
-
-        FirebaseInstallations.getInstance().id.addOnCompleteListener { task ->
-            firebaseInstallationId = if (task.isSuccessful) task.result else ""
-        }
     }
 
     @Throws(LeftoverBalanceException::class)
