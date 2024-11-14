dd3f704bf3f2107a7df83dbba3b98229ac4b0679
HashEngineering <hashengineeringsolutions@gmail.com>
2024-11-13 18:36:47 -0800

    style: ktlint

--- integrations/crowdnode/src/main/java/org/dash/wallet/integrations/crowdnode/model/CrowdNodeFeeInfo.kt
+++ integrations/crowdnode/src/main/java/org/dash/wallet/integrations/crowdnode/model/CrowdNodeFeeInfo.kt
@@ -5,7 +5,6 @@ import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.RawValue
 
-
 data class FeeLadder(
     val name: String,
     val type: String,
