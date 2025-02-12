8795d19a9f863b76576e82964e81cdba8184e2bd
HashEngineering <hashengineeringsolutions@gmail.com>
2025-02-12 07:50:39 -0800

    fix: log errors with more detail

--- wallet/src/de/schildbach/wallet/ui/LockScreenActivity.kt
+++ wallet/src/de/schildbach/wallet/ui/LockScreenActivity.kt
@@ -221,10 +221,10 @@ open class LockScreenActivity : SecureActivity() {
         super.onStart()
         autoLogout.setOnLogoutListener(onLogoutListener)
 
-        if (!keepUnlocked && configuration.autoLogoutEnabled &&
-            (autoLogout.keepLockedUntilPinEntered || autoLogout.shouldLogout())
-        ) {
-            log.info("onStart: $keepUnlocked && ${configuration.autoLogoutEnabled} && (${autoLogout.keepLockedUntilPinEntered} || ${autoLogout.shouldLogout()})")
+        val showLockScreen = !keepUnlocked && configuration.autoLogoutEnabled &&
+                (autoLogout.keepLockedUntilPinEntered || autoLogout.shouldLogout())
+        log.info("show lock screen $showLockScreen = !$keepUnlocked && ${configuration.autoLogoutEnabled} && (${autoLogout.keepLockedUntilPinEntered} || ${autoLogout.shouldLogout()})")
+        if (showLockScreen) {
             setLockState(
                 if (pinRetryController.isLocked) {
                     State.LOCKED
