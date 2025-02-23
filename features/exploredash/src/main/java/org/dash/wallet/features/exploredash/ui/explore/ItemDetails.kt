a6186bea81038c9da77c93be1ee085d4c2a068e5
Andrei Ashikhmin <synmcj@gmail.com>
2025-02-23 14:42:02 +0700

    fix: hide extra UI elements

--- features/exploredash/src/main/java/org/dash/wallet/features/exploredash/ui/explore/ItemDetails.kt
+++ features/exploredash/src/main/java/org/dash/wallet/features/exploredash/ui/explore/ItemDetails.kt
@@ -240,12 +240,14 @@ class ItemDetails(context: Context, attrs: AttributeSet) : LinearLayout(context,
 
             if (merchant.savingsAsDouble != 0.0) {
                 binding.discountValue.isVisible = true
+                binding.discountStem.isVisible = true
                 binding.discountValue.text = root.context.getString(
                     R.string.explore_pay_with_dash_save,
                     merchant.savingsPercentageAsDouble
                 )
             } else {
                 binding.discountValue.isVisible = false
+                binding.discountStem.isVisible = false
             }
         }
     }
