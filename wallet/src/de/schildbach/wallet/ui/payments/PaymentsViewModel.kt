f0490d4e19c6f49b078326533d18b04146293d74
HashEngineering <hashengineeringsolutions@gmail.com>
2025-05-06 15:45:51 -0700

    fix: add exception handling to PaymentsViewModel

--- wallet/src/de/schildbach/wallet/ui/payments/PaymentsViewModel.kt
+++ wallet/src/de/schildbach/wallet/ui/payments/PaymentsViewModel.kt
@@ -27,6 +27,7 @@ import de.schildbach.wallet.ui.dashpay.PlatformRepo
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
@@ -52,13 +53,17 @@ class PaymentsViewModel @Inject constructor(
     val dashPayProfile = _dashPayProfile.asLiveData()
 
     suspend fun getCurrentAddress() = withContext(Dispatchers.IO) {
-        Context.propagate(walletDataProvider.wallet!!.context)
-        walletDataProvider.currentReceiveAddress()
+        walletDataProvider.wallet?.let {
+            Context.propagate(it.context)
+            walletDataProvider.currentReceiveAddress()
+        } ?: error("Wallet not yet initialised")
     }
 
     suspend fun getFreshAddress() = withContext(Dispatchers.IO) {
-        Context.propagate(walletDataProvider.wallet!!.context)
-        walletDataProvider.freshReceiveAddress()
+        walletDataProvider.wallet?.let {
+            Context.propagate(it.context)
+            walletDataProvider.freshReceiveAddress()
+        } ?: error("Wallet not yet initialised")
     }
     init {
         identityConfig.observeBase()
@@ -66,7 +71,7 @@ class PaymentsViewModel @Inject constructor(
                 if (identity.userId != null && identity.creationComplete) {
                     platformRepo.observeProfileByUserId(identity.userId)
                 } else {
-                    flowOf()
+                    emptyFlow()
                 }
             }
             .onEach {
