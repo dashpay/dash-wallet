fad3e6d30a09bc52783e7043cba5eb57e955e5d2
HashEngineering <hashengineeringsolutions@gmail.com>
2025-10-05 22:24:37 -0700

    fix: include piggycards gift card transactions in the gift card filter

--- wallet/src/de/schildbach/wallet/ui/main/MainViewModel.kt
+++ wallet/src/de/schildbach/wallet/ui/main/MainViewModel.kt
@@ -768,7 +768,7 @@ class MainViewModel @Inject constructor(
     }
 
     private fun TransactionWrapper.isGiftCard(metadata: Map<Sha256Hash, PresentableTxMetadata>): Boolean {
-        return metadata[transactions.values.first().txId]?.service == ServiceName.CTXSpend
+        return ServiceName.isDashSpend(metadata[transactions.values.first().txId]?.service)
     }
 
     /**
