4d9b2a886c67b87745088b4104b1da20315acb0e
HashEngineering <hashengineeringsolutions@gmail.com>
2025-11-21 13:13:40 -0800

    fix: misc changes

--- wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.kt
+++ wallet/src/de/schildbach/wallet/service/BlockchainServiceImpl.kt
@@ -76,7 +76,6 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.withContext
 import org.bitcoinj.core.Address
