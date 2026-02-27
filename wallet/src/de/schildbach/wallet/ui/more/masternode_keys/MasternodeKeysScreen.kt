644cfa2badf1621722e549d853b959c82026ff93
HashEngineering <hashengineeringsolutions@gmail.com>
2026-02-26 19:20:29 -0800

    refactor: use ListItem in MasternodeKeysScreen

--- wallet/src/de/schildbach/wallet/ui/more/masternode_keys/MasternodeKeysScreen.kt
+++ wallet/src/de/schildbach/wallet/ui/more/masternode_keys/MasternodeKeysScreen.kt
@@ -18,33 +18,26 @@
 package de.schildbach.wallet.ui.more.masternode_keys
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import de.schildbach.wallet_test.R
 import kotlinx.coroutines.flow.StateFlow
+import org.dash.wallet.common.ui.components.ListItem
 import org.dash.wallet.common.ui.components.Menu
 import org.dash.wallet.common.ui.components.MyTheme
 import org.dash.wallet.common.ui.components.TopIntro
@@ -118,47 +111,20 @@ private fun MasternodeKeyTypeRow(
         }
     )
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(58.dp)
-            .clickable { onClick() }
-            .padding(horizontal = 10.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column(
-            modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.spacedBy(2.dp)
-        ) {
-            Text(
-                text = typeName,
-                style = MyTheme.Body2Medium,
-                color = MyTheme.Colors.textPrimary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+    ListItem(
+        title = typeName,
+        subtitle = stringResource(R.string.masternode_key_type_total, info.totalKeys),
+        trailingText = stringResource(R.string.masternode_key_type_used, info.usedKeys),
+        trailingTrailingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_menu_row_arrow),
+                contentDescription = null,
+                tint = MyTheme.Colors.textTertiary,
+                modifier = Modifier.size(16.dp)
             )
-            Text(
-                text = stringResource(R.string.masternode_key_type_total, info.totalKeys),
-                style = MyTheme.OverlineMedium,
-                color = MyTheme.Colors.textSecondary,
-                maxLines = 1
-            )
-        }
-
-        Text(
-            text = stringResource(R.string.masternode_key_type_used, info.usedKeys),
-            style = MyTheme.Caption,
-            color = MyTheme.Colors.textSecondary,
-            modifier = Modifier.padding(end = 10.dp)
-        )
-
-        Icon(
-            painter = painterResource(id = R.drawable.ic_menu_row_arrow),
-            contentDescription = null,
-            tint = MyTheme.Colors.textTertiary,
-            modifier = Modifier.size(16.dp)
-        )
-    }
+        },
+        onClick = onClick
+    )
 }
 
 @Preview(showBackground = true)
